f1e2216fa6eb7b75b4894fea0a2e7aebb3b2568e
SungHoHong2 <maverickjin88@gmail.com>
2017-10-25 14:55:41 -0700

    add

--- messenger/librados_Experiment/hello_world.cc
+++ messenger/librados_Experiment/hello_world.cc
@@ -85,10 +85,11 @@ int main(int argc, const char **argv)
   }
 
 
-  librados::IoCtx io_ctx;
-  const char *pool_name = "data";
-  ret = cluster.ioctx_create(pool_name, io_ctx);
-
+  {
+    librados::IoCtx io_ctx;
+    const char *pool_name = "data";
+    ret = cluster.ioctx_create(pool_name, io_ctx);
+  }
 
   /*
    * let's create our own pool instead of scribbling over real data.
