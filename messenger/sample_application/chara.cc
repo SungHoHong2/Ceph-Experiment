d8143bf61b7e3a5d67d6ea83e5f25d52a78a692a
SungHoHong2 <maverickjin88@gmail.com>
2017-10-25 15:36:51 -0700

    add

--- messenger/sample_application/chara.cc
+++ messenger/sample_application/chara.cc
@@ -1,9 +1,15 @@
 #include <rados/librados.hpp>
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main(int argc, const char **argv){
   int ret = 0;
+
+  const char *pool_name = "hello_world_pool";
+  string hello("hello world!");
+  string object_name("hello_object");
+  librados::IoCtx io_ctx;
 
 
 
