711fa9f8996047c8006fe1378fda4f735c7cd6f2
SungHoHong2 <maverickjin88@gmail.com>
2017-10-31 10:08:36 -0700

    add

--- ceph-deploy/ceph_monitor_install.sh
+++ ceph-deploy/ceph_monitor_install.sh
@@ -1,3 +1,10 @@
+monitor_name="monitortest"
+address="10.143.3.131"
+fileuuid="f9a8ed78-92aa-4c20-a42e-bf97431698cf"
+
+mkdir /etc/ceph
+cp ceph.conf /etc/ceph/ceph.conf
+
 ceph-authtool --create-keyring /etc/ceph/ceph.mon.keyring --gen-key -n mon. --cap mon 'allow *'
 
 wait
@@ -10,11 +17,11 @@ ceph-authtool /etc/ceph/ceph.mon.keyring --import-keyring /etc/ceph/ceph.client.
 
 wait
 
-monmaptool --create --add monitortest 10.143.2.14 --fsid f9a8ed78-92aa-4c20-a42e-bf97431698cf /etc/ceph/monmap
+monmaptool --create --add ${monitor_name} ${address} --${fileuuid} /etc/ceph/monmap
 
 wait
 
-mkdir -p /var/lib/ceph/mon/ceph-monitortest
+mkdir -p /var/lib/ceph/mon/ceph-${monitor_name}
 
 wait
 
@@ -22,5 +29,32 @@ ceph-mon --mkfs -i monitor01 --monmap /etc/ceph/monmap --keyring /etc/ceph/ceph.
 
 wait
 
-touch /var/lib/ceph/mon/ceph-monitortest/done
-touch /var/lib/ceph/mon/ceph-monitortest/upstart
+touch /var/lib/ceph/mon/ceph-${monitor_name}/done
+touch /var/lib/ceph/mon/ceph-${monitor_name}/upstart
+
+# ceph-authtool --create-keyring /etc/ceph/ceph.mon.keyring --gen-key -n mon. --cap mon 'allow *'
+#
+# wait
+#
+# ceph-authtool --create-keyring /etc/ceph/ceph.client.admin.keyring --gen-key -n client.admin --set-uid=0 --cap mon 'allow *' --cap osd 'allow *' --cap mds 'allow *' --cap mgr 'allow *'
+#
+# wait
+#
+# ceph-authtool /etc/ceph/ceph.mon.keyring --import-keyring /etc/ceph/ceph.client.admin.keyring
+#
+# wait
+#
+# monmaptool --create --add monitortest 10.143.2.14 --fsid f9a8ed78-92aa-4c20-a42e-bf97431698cf /etc/ceph/monmap
+#
+# wait
+#
+# mkdir -p /var/lib/ceph/mon/ceph-monitortest
+#
+# wait
+#
+# ceph-mon --mkfs -i monitor01 --monmap /etc/ceph/monmap --keyring /etc/ceph/ceph.mon.keyring
+#
+# wait
+#
+# touch /var/lib/ceph/mon/ceph-monitortest/done
+# touch /var/lib/ceph/mon/ceph-monitortest/upstart
