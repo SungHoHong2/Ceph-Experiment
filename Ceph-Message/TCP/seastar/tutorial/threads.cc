c9eb3800a745331a005c1ad4cff1964bfa27b5c9
SungHoHong2 <maverickjin88@gmail.com>
2018-02-11 17:14:06 -0700

    add

--- Ceph-Message/TCP/seastar/tutorial/threads.cc
+++ Ceph-Message/TCP/seastar/tutorial/threads.cc
@@ -2,11 +2,12 @@
 #include "core/reactor.hh"
 #include <iostream>
 using namespace seastar;
+using namespace std;
 
 int main(int argc, char** argv) {
     app_template app;
     app.run(argc, argv, [] {
-            std::cout << "Hello world\n";
+            cout << "Hello world  " << smp::count <<  endl;  // printout
             return make_ready_future<>();
     });
 }
