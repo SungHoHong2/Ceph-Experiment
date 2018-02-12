3a85c4b266528ab3a1ef13d1cb503ccd71761c42
SungHoHong2 <maverickjin88@gmail.com>
2018-02-11 17:15:49 -0700

    add

--- Ceph-Message/TCP/seastar/tutorial/threads.cc
+++ Ceph-Message/TCP/seastar/tutorial/threads.cc
@@ -6,8 +6,15 @@ using namespace std;
 
 int main(int argc, char** argv) {
     app_template app;
+
+    try{
     app.run(argc, argv, [] {
-            cout << "Hello world  " << smp::count <<  endl;  // printout
+            cout << "Hello world  " << smp::count <<  endl;  // prints out the total number of running threads
             return make_ready_future<>();
     });
+  } catch(runtime_error &e){
+        cerr << "Couldn't start application: " << e.what() << "\n";
+        return 1;
+  }
+
 }
