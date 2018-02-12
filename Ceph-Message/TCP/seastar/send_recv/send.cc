9169f7a8da228949918d5d49a55bb4da549f8a44
SungHoHong2 <maverickjin88@gmail.com>
2018-02-11 19:37:24 -0700

    add

--- Ceph-Message/TCP/seastar/send_recv/send.cc
+++ Ceph-Message/TCP/seastar/send_recv/send.cc
@@ -11,8 +11,20 @@ using namespace seastar;
 using namespace std;
 namespace bpo = boost::program_options;
 
+
+template <typename... Args>
+void http_debug(const char* fmt, Args&&... args) { // what does this ... represent? is there a good phrase so I can learn more about this in google?
+#if HTTP_DEBUG
+    print(fmt, std::forward<Args>(args)...);
+#endif
+}
+
+
 int main(int ac, char** av) {
   app_template app;
+  cout << "program start" << endl;
+
+  // this part automatically gets parses all the arguments for you
   app.add_options()
       ("server,s", bpo::value<std::string>()->default_value("10.218.111.252:10000"), "Server address")
       ("conn,c", bpo::value<unsigned>()->default_value(100), "total connections")
@@ -20,7 +32,32 @@ int main(int ac, char** av) {
       ("duration,d", bpo::value<unsigned>()->default_value(10), "duration of the test in seconds)");
 
 
+  return app.run(ac, av, [&app] () -> future<int> { // what does this part actually mean? the app already has a pointer?
+                                                    // because app-template.hh does not have any direct a future function and I am
+                                                    // wondering hiw did this is possible in the first place
+    auto& config = app.configuration();
+    auto server = config["server"].as<std::string>(); // server ip address
+    auto reqs_per_conn = config["reqs"].as<unsigned>(); // requests per connection
+    auto total_conn= config["conn"].as<unsigned>(); // number of connections
 
-  cout << "program ended" << endl;
-  return 0;
+    if (total_conn % smp::count != 0) { // future count should have numbers
+        print("Error: conn needs to be n * cpu_nr\n");
+        return make_ready_future<int>(-1);
+    }
+
+    auto http_clients = new distributed<http_client>;
+    // Start http requests on all the cores
+    auto started = steady_clock_type::now();
+    return http_clients->start(std::move(duration), std::move(total_conn), std::move(reqs_per_conn)).then([http_clients, server] {
+      return http_clients->invoke_on_all(&http_client::connect, ipv4_addr{server});
+    });
+
+
+
+
+
+
+
+    cout << "program ended" << endl;
+  });
 }
