d5681dc74c11420621f4c044abd07f7b8f0b935d
SungHoHong2 <maverickjin88@gmail.com>
2018-02-16 12:04:52 -0700

    add

--- Ceph-Message/TCP/seastar/send_recv/send.cc
+++ Ceph-Message/TCP/seastar/send_recv/send.cc
@@ -9,6 +9,8 @@
 #include <chrono>
 using namespace seastar;
 using namespace std;
+using namespace http_client;
+
 namespace bpo = boost::program_options;
 
 
@@ -93,7 +95,7 @@ int main(int ac, char** av) {
     // Start http requests on all the cores
     auto started = steady_clock_type::now();
     return http_clients->start(move(duration), move(total_conn), move(reqs_per_conn)).then([http_clients, server] {
-      return http_clients->invoke_on_all(&http_client::connect, ipv4_addr{server});
+      return http_clients->invoke_on_all(&connect, ipv4_addr{server});
     });
 
 
