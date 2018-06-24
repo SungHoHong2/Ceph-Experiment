146bce231c6063a7cbead074ff688a0594c750f4
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-24 23:15:43 +0900

    $HISTORY

--- Ceph-Seastar/version.sh
+++ Ceph-Seastar/version.sh
@@ -5,7 +5,7 @@ if [ "$1" = "host_to_git" ]
 then
 rm -rf history/$HISTORY
 mkdir history/$HISTORY
-cp /Users/sunghohong/Documents/Projects/ceph/src/Makefile src/Makefile
+# cp /Users/sunghohong/Documents/Projects/ceph/src/Makefile src/Makefile
 cp /Users/sunghohong/Documents/Projects/ceph/src/ceph_osd.cc src/ceph_osd.cc
 
 
@@ -20,7 +20,7 @@ git push origin master
 elif [ "$1" = "git_to_server" ]
 then
 git pull origin master
-cp /home/sungho/Ceph-Experiment/Ceph-Seastar/src/Makefile /home/sungho/ceph/build/
+# cp /home/sungho/Ceph-Experiment/Ceph-Seastar/src/Makefile /home/sungho/ceph/build/
 cp /home/sungho/Ceph-Experiment/Ceph-Seastar/src/ceph_osd.cc /home/sungho/ceph/src/
 cp /home/sungho/Ceph-Experiment/Ceph-Seastar/src/ceph_osd.cc /home/sungho/ceph/build/src
 
