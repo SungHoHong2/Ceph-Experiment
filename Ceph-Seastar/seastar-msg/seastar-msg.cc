eb1bc7cc2d49b9d48b17a75dd967fcdcba4d2b0a
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-26 16:50:03 +0900

    $HISTORY

--- Ceph-Seastar/seastar-msg/seastar-msg.cc
+++ Ceph-Seastar/seastar-msg/seastar-msg.cc
@@ -13,10 +13,33 @@
 #include "client.hh"
 
 namespace bpo = boost::program_options;
-std::string arg_host;
 
 
-void server_task(int ac, char** av){
+void server_task(){}
+
+
+int main(int ac, char** av) {
+    std::cout << "MAIN BEGIN" << std::endl;
+
+
+    char hostname[150];
+    gethostname(hostname, 150);
+
+    std::string arg_host;
+
+    if(strcmp("wenji-w1",hostname)==0){
+        arg_host = "10.218.104.170:1234";
+
+
+    }else if(strcmp("w2",hostname)==0){
+        arg_host = "10.218.111.252:1234";
+
+
+    } else {
+        std::cout << "wrong host" << std::endl;
+        return 0;
+    }
+
 
     app_template app;
     app.add_options()
@@ -26,6 +49,17 @@ void server_task(int ac, char** av){
             ("conn", bpo::value<unsigned>()->default_value(1), "nr connections per cpu")
             ("proto", bpo::value<std::string>()->default_value("tcp"), "transport protocol tcp|sctp")
             ("smp", bpo::value<unsigned>()->default_value(1), "smp");
+
+
+    char _args[] = "Hello";
+    // std::thread t1(task1, _args);
+
+
+    std::thread tserver(server_task);
+    std::thread tclient(client_task);
+
+
+
 
     app.run_deprecated(ac, av, [&app] {
         auto&& config = app.configuration();
@@ -51,48 +85,6 @@ void server_task(int ac, char** av){
         });
 
 
-//        using namespace std::chrono_literals;
-//        sleep(10s).then([conn_server, test, ncon] {
-//            clients.start().then([conn_server, test, ncon] () {
-//                clients.invoke_on_all(&client::start, ipv4_addr{conn_server}, test, ncon);
-//
-//
-//            });
-//        });
-
-        std::cout << "MAIN END" << std::endl;
-    });
-
-
-}
-
-
-void client_task(int ac, char** av){
-    app_template app2;
-    app2.add_options()
-            ("server", bpo::value<std::string>()->default_value(arg_host), "Server address")
-            ("port", bpo::value<uint16_t>()->default_value(1234), "TCP server port")
-            ("test", bpo::value<std::string>()->default_value("ping"), "test type(ping | rxrx | txtx)")
-            ("conn", bpo::value<unsigned>()->default_value(1), "nr connections per cpu")
-            ("proto", bpo::value<std::string>()->default_value("tcp"), "transport protocol tcp|sctp")
-            ("smp", bpo::value<unsigned>()->default_value(1), "smp");
-
-    app2.run_deprecated(ac, av, [&app2] {
-        auto&& config = app2.configuration();
-        auto conn_server = config["server"].as<std::string>();
-        auto test = config["test"].as<std::string>();
-        auto ncon = config["conn"].as<unsigned>();
-        auto proto = config["proto"].as<std::string>();
-        uint16_t port = config["port"].as<uint16_t>();
-
-        protocol = transport::TCP;
-        enable_tcp = 1;
-
-        if (!client::tests.count(test)) {
-            return engine().exit(1);
-        }
-
-
         using namespace std::chrono_literals;
         sleep(10s).then([conn_server, test, ncon] {
             clients.start().then([conn_server, test, ncon] () {
@@ -104,37 +96,7 @@ void client_task(int ac, char** av){
 
         std::cout << "MAIN END" << std::endl;
     });
-}
 
-
-int main(int ac, char** av) {
-    std::cout << "MAIN BEGIN" << std::endl;
-
-
-    char hostname[150];
-    gethostname(hostname, 150);
-
-    if(strcmp("wenji-w1",hostname)==0){
-        arg_host = "10.218.104.170:1234";
-
-
-    }else if(strcmp("w2",hostname)==0){
-        arg_host = "10.218.111.252:1234";
-
-
-    } else {
-        std::cout << "wrong host" << std::endl;
-        return 0;
-    }
-
-
-
-    char _args[] = "Hello";
-    std::thread t1(task1, _args);
-
-
-    std::thread tserver(server_task, ac, av);
-    std::thread tclient(client_task, ac, av);
 
 
 
