5e531ff0aef22dcba999775d8b42ddd56dee7de2
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-26 16:37:57 +0900

    $HISTORY

--- Ceph-Seastar/seastar-msg/seastar-msg.cc
+++ Ceph-Seastar/seastar-msg/seastar-msg.cc
@@ -16,14 +16,75 @@ namespace bpo = boost::program_options;
 
 
 void server_task(){
+    app.run_deprecated(ac, av, [&app] {
+        auto&& config = app.configuration();
+        auto conn_server = config["server"].as<std::string>();
+        auto test = config["test"].as<std::string>();
+        auto ncon = config["conn"].as<unsigned>();
+        auto proto = config["proto"].as<std::string>();
+        uint16_t port = config["port"].as<uint16_t>();
+
+        protocol = transport::TCP;
+        enable_tcp = 1;
+
+
+        auto server = new distributed<tcp_server>; // run distributed object
+        server->start().then([server = std::move(server), port] () mutable {
+            engine().at_exit([server] {
+                return server->stop();
+            });
+            server->invoke_on_all(&tcp_server::listen, ipv4_addr{port});
+            // Invoke a method on all Service instances in parallel.
+        }).then([port] {
+            std::cout << "Seastar TCP server listening on port " << port << " ...\n";
+        });
+
+
+//        using namespace std::chrono_literals;
+//        sleep(10s).then([conn_server, test, ncon] {
+//            clients.start().then([conn_server, test, ncon] () {
+//                clients.invoke_on_all(&client::start, ipv4_addr{conn_server}, test, ncon);
+//
+//
+//            });
+//        });
+
+        std::cout << "MAIN END" << std::endl;
+    });
 
 
 }
 
 
 void client_task(){
+    app_template app2;
+    return app2.run_deprecated(ac, av, [&app2] {
+        auto&& config = app2.configuration();
+        auto conn_server = config["server"].as<std::string>();
+        auto test = config["test"].as<std::string>();
+        auto ncon = config["conn"].as<unsigned>();
+        auto proto = config["proto"].as<std::string>();
+        uint16_t port = config["port"].as<uint16_t>();
+
+        protocol = transport::TCP;
+        enable_tcp = 1;
+
+        if (!client::tests.count(test)) {
+            return engine().exit(1);
+        }
 
 
+        using namespace std::chrono_literals;
+        sleep(10s).then([conn_server, test, ncon] {
+            clients.start().then([conn_server, test, ncon] () {
+                clients.invoke_on_all(&client::start, ipv4_addr{conn_server}, test, ncon);
+
+
+            });
+        });
+
+        std::cout << "MAIN END" << std::endl;
+    });
 }
 
 
@@ -64,77 +125,15 @@ int main(int ac, char** av) {
     std::thread t1(task1, _args);
 
 
-//    std::thread tserver(server_task);
-//    std::thread tclient(client_task);
+    std::thread tserver(server_task);
+    std::thread tclient(client_task);
 
 
 
 
-    app.run_deprecated(ac, av, [&app] {
-        auto&& config = app.configuration();
-        auto conn_server = config["server"].as<std::string>();
-        auto test = config["test"].as<std::string>();
-        auto ncon = config["conn"].as<unsigned>();
-        auto proto = config["proto"].as<std::string>();
-        uint16_t port = config["port"].as<uint16_t>();
-
-        protocol = transport::TCP;
-        enable_tcp = 1;
 
 
-        auto server = new distributed<tcp_server>; // run distributed object
-        server->start().then([server = std::move(server), port] () mutable {
-            engine().at_exit([server] {
-                return server->stop();
-            });
-            server->invoke_on_all(&tcp_server::listen, ipv4_addr{port});
-            // Invoke a method on all Service instances in parallel.
-        }).then([port] {
-            std::cout << "Seastar TCP server listening on port " << port << " ...\n";
-        });
 
-
-//        using namespace std::chrono_literals;
-//        sleep(10s).then([conn_server, test, ncon] {
-//            clients.start().then([conn_server, test, ncon] () {
-//                clients.invoke_on_all(&client::start, ipv4_addr{conn_server}, test, ncon);
-//
-//
-//            });
-//        });
-
-        std::cout << "MAIN END" << std::endl;
-    });
-
-
-    app_template app2;
-    return app2.run_deprecated(ac, av, [&app2] {
-        auto&& config = app2.configuration();
-        auto conn_server = config["server"].as<std::string>();
-        auto test = config["test"].as<std::string>();
-        auto ncon = config["conn"].as<unsigned>();
-        auto proto = config["proto"].as<std::string>();
-        uint16_t port = config["port"].as<uint16_t>();
-
-        protocol = transport::TCP;
-        enable_tcp = 1;
-
-        if (!client::tests.count(test)) {
-            return engine().exit(1);
-        }
-
-
-        using namespace std::chrono_literals;
-        sleep(10s).then([conn_server, test, ncon] {
-            clients.start().then([conn_server, test, ncon] () {
-                clients.invoke_on_all(&client::start, ipv4_addr{conn_server}, test, ncon);
-
-
-            });
-        });
-
-        std::cout << "MAIN END" << std::endl;
-    });
 
 
 
