6f8474ad07d8cdebfbb806bb243946eaff7c03ad
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-06-29 13:49:19 -0700

    seastar_msg_to_git

--- Ceph-Seastar/history/1_seastar_msg/server.hh
+++ Ceph-Seastar/history/1_seastar_msg/server.hh
@@ -67,7 +67,7 @@ public:
             return read();
         }
         future<> read() {
-            sleep(1);
+            sleep(0);
             if (_read_buf.eof()) {
                 return make_ready_future();
             }
@@ -78,7 +78,7 @@ public:
                     memset(recv_packet,0, PACKET_SIZE);
                     memcpy(recv_packet, buf.get(),  buf.size());
 
-                    // if(strcmp("^^^^^", recv_packet) != 0)
+                    if(strcmp("^^^^^", recv_packet) != 0)
                     std::cout << "LISTEN::" << recv_packet << "  " << buf.size() <<  std::endl;
                 }
                 return this->read();
