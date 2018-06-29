9ad76e0daaa2c196a472b228d8628e6c1545e563
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-06-29 14:45:43 -0700

    seastar_msg_to_git

--- Ceph-Seastar/history/1_seastar_msg/server.hh
+++ Ceph-Seastar/history/1_seastar_msg/server.hh
@@ -66,7 +66,7 @@ public:
                 , _read_buf(_fd.input())
                 , _write_buf(_fd.output()) {}
         future<> process() {
-            return when_all(read());
+            return read();
         }
         future<> read() {
             usleep(0);
@@ -86,6 +86,6 @@ public:
                 return this->read();
             });
         }
-        
+
     };
 };
