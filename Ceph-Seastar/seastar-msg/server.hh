d0fcff7726c022067395654c736fc98fa71fb24c
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-06-29 13:59:35 -0700

    seastar_msg_to_git

--- Ceph-Seastar/seastar-msg/server.hh
+++ Ceph-Seastar/seastar-msg/server.hh
@@ -75,11 +75,11 @@ public:
             return _read_buf.read_exactly(5).then([this] (temporary_buffer<char> buf) {
 
                 if(buf.size()>2) {
-                    memset(recv_packet,0, PACKET_SIZE);
-                    memcpy(recv_packet, buf.get(),  buf.size());
+                   // memset(recv_packet,0, PACKET_SIZE);
+                   // memcpy(recv_packet, buf.get(),  buf.size());
 
                     // if(strcmp("^^^^^", recv_packet) != 0)
-                    std::cout << "LISTEN::" << recv_packet << "  " << buf.size() <<  std::endl;
+                    std::cout << "LISTEN::" << buf.get() << "  " << buf.size() <<  std::endl;
                 }
                 return this->read();
             });
