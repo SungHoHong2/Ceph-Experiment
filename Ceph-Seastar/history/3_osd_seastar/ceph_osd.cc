291ef50756ce13f9ecc9175329cd870fd5986f43
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-01 16:37:00 -0700

    $HISTORY

--- Ceph-Seastar/history/3_osd_seastar/ceph_osd.cc
+++ Ceph-Seastar/history/3_osd_seastar/ceph_osd.cc
@@ -52,13 +52,13 @@
 #define dout_context g_ceph_context
 #define dout_subsys ceph_subsys_osd
 
-#include "core/reactor.hh"
-#include "core/app-template.hh"
-#include "core/temporary_buffer.hh"
-#include "core/future-util.hh"
-#include "core/distributed.hh"
-#include "core/sleep.hh"
-#include <vector>
+//#include "core/reactor.hh"
+//#include "core/app-template.hh"
+//#include "core/temporary_buffer.hh"
+//#include "core/future-util.hh"
+//#include "core/distributed.hh"
+//#include "core/sleep.hh"
+//#include <vector>
 
 char hostname[150];
 namespace {
