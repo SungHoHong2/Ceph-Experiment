06866a8f7ae8b3f0255f3b81aae79db49859baa3
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-03 09:54:40 -0700

    $HISTORY

--- Ceph-Seastar/history/4_osd_seastar/ceph_osd.cc
+++ Ceph-Seastar/history/4_osd_seastar/ceph_osd.cc
@@ -1,3 +1,17 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2004-2006 Sage Weil <sage@newdream.net>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -29,6 +43,8 @@
 #include "common/errno.h"
 #include "common/pick_address.h"
 
+#include "perfglue/heap_profiler.h"
+
 #include "include/assert.h"
 
 #include "common/Preforker.h"
@@ -43,13 +59,6 @@
 #include "core/distributed.hh"
 #include "core/sleep.hh"
 #include <vector>
-
-
-
-namespace bpo = boost::program_options;
-using namespace seastar;
-
-
 
 char hostname[150];
 namespace {
