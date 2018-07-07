bae8d104aa0047ed5bff64afa5c8b1e82fad1b9a
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-07 14:49:31 -0700

    add

--- Seastar-FUSE/version.sh
+++ Seastar-FUSE/version.sh
@@ -35,16 +35,16 @@ then
 FILENAME_ARG="/dev/sdb"
 
 cd /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar
-rm -rf /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar/temp_file && touch /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar/temp_file # | grep "READ:"
+TEMP_FILE_LOG="/home/sungho/Ceph-Experiment/Seastar-FUSE/temp_file"
+rm -rf $TEMP_FILE_LOG && touch $TEMP_FILE_LOG # | grep "READ:"
 
 echo "READ" >> temp_file
-fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randread -bs=4k -numjobs=1 -size 30M  --group_reporting -name=mytest | grep "READ:" >> /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar/temp_file
-wait
+fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randread -bs=4k -numjobs=1 -size 30M  --group_reporting -name=mytest | grep "READ:" >> $TEMP_FILE_LOG
 echo "WRITE" >> temp_file
-fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randwrite -bs=4k -numjobs=1 -size 30M  --group_reporting -name=mytest | grep "WRITE:" >> /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar/temp_file
+fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randwrite -bs=4k -numjobs=1 -size 30M  --group_reporting -name=mytest | grep "WRITE:" >> $TEMP_FILE_LOG
 wait
 echo "RANDRW" >> temp_file
-fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randrw -bs=4k -numjobs=1 -size 30M  --group_reporting -name=mytest | grep "WRITE:|READ:" >> /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar/temp_file
+fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randrw -bs=4k -numjobs=1 -size 30M  --group_reporting -name=mytest | grep "WRITE:|READ:" >> $TEMP_FILE_LOG
 
 
 # sudo fio --name=randwrite --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=0 --size=512M --numjobs=2 --runtime=240 --group_reporting
