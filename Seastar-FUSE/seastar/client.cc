fd322946ea18f1847e4aec02e325ae16ca324b34
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-07 12:35:30 -0700

    add

--- Seastar-FUSE/seastar/client.cc
+++ Seastar-FUSE/seastar/client.cc
@@ -11,7 +11,6 @@ static int tx_msg_total_size = 100 * 1024 * 1024;
 static int tx_msg_size = 4 * 1024;
 static int tx_msg_nr = tx_msg_total_size / tx_msg_size;
 static std::string str_txbuf(tx_msg_size, 'X');
-static int total_ping_identifier = 0;
 
 class client;
 distributed<client> clients;
@@ -223,7 +222,7 @@ int main(int ac, char ** av) {
             ("server", bpo::value<std::string>()->default_value("192.168.56.101:1234"), "Server address")
             ("test", bpo::value<std::string>()->default_value("ping"), "test type(ping | rxrx | txtx)")
             ("conn", bpo::value<unsigned>()->default_value(1), "nr connections per cpu")
-            ("proto", bpo::value<std::string>()->default_value("tcp"), "transport protocol tcp|sctp")
+            ("proto", bpo::value<std::string>()->default_value("tcp"), "transport protocol tcp|sctp");
 
     return app.run_deprecated(ac, av, [&app] {
         auto&& config = app.configuration();
