afacae0172d088ee3401c8c55dd26307e4ea4043
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-07 14:55:45 -0700

    add

--- Seastar-FUSE/version.sh
+++ Seastar-FUSE/version.sh
@@ -43,7 +43,7 @@ fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randread -bs=4k -nu
 echo "WRITE" >>  $TEMP_FILE_LOG
 fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randwrite -bs=4k -numjobs=1 -size 30M  --group_reporting -name=mytest | grep "WRITE:" >> $TEMP_FILE_LOG
 echo "RANDRW" >> $TEMP_FILE_LOG
-fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randrw -bs=4k -numjobs=1 -size 30M  --group_reporting -name=mytest | grep "WRITE:|READ:" >> $TEMP_FILE_LOG
+fio -filename=$FILENAME_ARG -direct=1 -iodepth=1 -thread -rw=randrw -bs=4k -numjobs=1 -size 30M  --group_reporting -name=mytest | grep "WRITE:" | grep "READ:" >> $TEMP_FILE_LOG
 
 
 # sudo fio --name=randwrite --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=0 --size=512M --numjobs=2 --runtime=240 --group_reporting
