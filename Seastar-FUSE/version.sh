6c768459da612cb306cae18f1d11094b082f2ae6
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-07 11:13:42 -0700

    add

--- Seastar-FUSE/version.sh
+++ Seastar-FUSE/version.sh
@@ -11,9 +11,10 @@ cd /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar
       then
       g++-5  `pkg-config --cflags --libs /home/sungho/seastar/build/release/seastar.pc` server.cc -o server
 
-      elif [ "$HOSTS" = "wenji-w1" ]
+      elif [ "$HOSTS" = "w1" ]
       then
       g++ `pkg-config --cflags --libs /home/sungho/seastar/build/release/seastar.pc` client.cc -o client
+      fi
 
 elif [ "$1" = "seastar_make" ]
 cd /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar
@@ -21,9 +22,11 @@ cd /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar
     then
     ./server --dpdk-pmd --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.253 --netmask-ipv4-addr 255.255.248.0 --gw-ipv4-addr 10.218.111.1 --collectd 0 --smp 1
 
-    elif [ "$HOSTS" = "wenji-w1" ]
+    elif [ "$HOSTS" = "w1" ]
     then
     ./client --dpdk-pmd --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.254 --netmask-ipv4-addr 255.255.248.0 --gw-ipv4-addr 10.218.111.1 --collectd 0 --smp 1
+    fi
+
 else
 echo "no argument"
 
