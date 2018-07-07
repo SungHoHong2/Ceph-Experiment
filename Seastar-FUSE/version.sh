c71b007fc0b889b5b51119de8d05d88263281c7d
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-07 11:51:46 -0700

    add

--- Seastar-FUSE/version.sh
+++ Seastar-FUSE/version.sh
@@ -7,13 +7,13 @@ then
 git pull origin master
 cd /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar
 # ./configure.py --enable-dpdk --compiler g++-5 --mode release
-      if [ "$HOSTS" = "w2" ]
+      if [ "$HOSTS" = "w2" ] # DK
       then
-      g++-5  `pkg-config --cflags --libs /home/sungho/seastar/build/release/seastar.pc` server.cc -o server
+      g++-5  `pkg-config --cflags --libs /home/sungho/seastar/build/release/seastar.pc` client.cc -o client
 
-      elif [ "$HOSTS" = "w1" ]
+      elif [ "$HOSTS" = "w1" ] # WENJI
       then
-      g++ `pkg-config --cflags --libs /home/sungho/seastar/build/release/seastar.pc` client.cc -o client
+      g++ `pkg-config --cflags --libs /home/sungho/seastar/build/release/seastar.pc` server.cc -o server
       fi
 
 elif [ "$1" = "seastar_run" ]
@@ -21,7 +21,7 @@ then
 cd /home/sungho/Ceph-Experiment/Seastar-FUSE/seastar
     if [ "$HOSTS" = "w2" ]
     then
-    ./server --dpdk-pmd --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.253 --netmask-ipv4-addr 255.255.248.0 --gw-ipv4-addr 10.218.111.1 --collectd 0 --smp 1
+    ./server --dpdk-pmd --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.253 --netmask-ipv4-addr 255.255.248.0 --gw-ipv4-addr 10.218.111.1 --collectd 0 --server "10.218.111.254:1234" --smp 1
 
     elif [ "$HOSTS" = "w1" ]
     then
