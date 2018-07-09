97de989fb431470ac9559e887de33fbbe3b7fd55
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-09 15:29:08 -0700

    add

--- Seastar-FUSE/version.sh
+++ Seastar-FUSE/version.sh
@@ -119,7 +119,9 @@ then
 git pull origin master
 FUSE_DIR=`pkg-config fuse --cflags --libs`
 
-g++ -X $FUSE_DIR fuse/stupid.cc -o fuse/stupid && fuse/stupid
+g++ $FUSE_DIR fuse/stupid.cc -o fuse/stupid && fuse/stupid
+
+gcc $FUSE_DIR fuse/stupid.c -o fuse/stupid && fuse/stupid
 
 
 
