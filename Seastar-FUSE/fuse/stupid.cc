0f195177bab430d68838a793dbc479c70889019c
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-09 15:01:27 -0700

    add

--- Seastar-FUSE/fuse/stupid.cc
+++ Seastar-FUSE/fuse/stupid.cc
@@ -1,3 +1,13 @@
+#define FUSE_USE_VERSION 30
+
+#include <fuse.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include <iostream>
 
 int main(){
