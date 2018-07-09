26cd15e19c7b14f95458b201269c6897767a9217
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-09 15:29:46 -0700

    add

--- Seastar-FUSE/fuse/stupid.c
+++ Seastar-FUSE/fuse/stupid.c
@@ -95,6 +95,5 @@ static struct fuse_operations operations = {
 
 int main( int argc, char *argv[] ){
 
-  std::cout << "FUSE BEGIN" << std::endl;
   return fuse_main( argc, argv, &operations, NULL );
 }
