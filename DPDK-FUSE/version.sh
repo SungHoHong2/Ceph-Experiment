46bb087e29fec0a3d1a6b061d441fc33418d136a
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-13 15:49:08 -0700

    add

--- DPDK-FUSE/version.sh
+++ DPDK-FUSE/version.sh
@@ -31,13 +31,13 @@ elif [ "$1" = "dpdk_server_make" ]
 then
 cd /home/sungho/Ceph-Experiment/DPDK-FUSE/dpdk-server/
 make
-./build/dpdk-server -l 0-3 -n 4 -- -q 8 -p 0x1 -T 1
+./build/dpdk-server -c 0x2 -n 4 -- -q 8 -p 0x1 -T 1
 
 elif [ "$1" = "dpdk_client_make" ]
 then
 cd /home/sungho/Ceph-Experiment/DPDK-FUSE/dpdk-client/
 make
-./build/dpdk-client -l 0-3 -n 4 -- -q 8 -p 0x1 -T 1
+./build/dpdk-client -c 0x2 -n 4 -- -q 8 -p 0x1 -T 1
 
 else
 echo "no argument"
