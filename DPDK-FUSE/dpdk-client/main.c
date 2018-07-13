4187016f7475111725800ddf245c9c3900bc2dd8
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-13 16:11:54 -0700

    add

--- DPDK-FUSE/dpdk-client/main.c
+++ DPDK-FUSE/dpdk-client/main.c
@@ -174,10 +174,10 @@ l2fwd_simple_forward(struct rte_mbuf *m, unsigned portid)
 	// sleep(1);
 	// printf("pkt_len: %d\n",pkt_len);
 
-	char *_m;
-	_m = rte_pktmbuf_append(m, 1024);
-
-	pkt_len = rte_pktmbuf_pkt_len(m);
+	//	char *_m;
+	//	_m = rte_pktmbuf_append(m, 1024);
+	//
+	//	pkt_len = rte_pktmbuf_pkt_len(m);
 	// printf("pkt_len after: %d\n",pkt_len);
 
 	// memset(_m, '*', rte_pktmbuf_pkt_len(m));
