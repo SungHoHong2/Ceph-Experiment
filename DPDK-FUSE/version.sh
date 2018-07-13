4fb9f6c65050d970aca41bdc52fb086d27dc5097
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-12 20:23:11 -0700

    add

--- DPDK-FUSE/version.sh
+++ DPDK-FUSE/version.sh
@@ -5,20 +5,25 @@ git pull origin master
 
 
 if [ "$1" = "multiprocess_make"]
+then
 cd /home/sungho/Ceph-Experiment/DPDK-FUSE/multi_process
 make
 elif ["$1" = "multiprocess_simple_mp"]
+then
 /home/sungho/Ceph-Experiment/DPDK-FUSE/multi_process/simple_mp/build/simple_mp -c 3 -n 4 --proc-type=primary
 /home/sungho/Ceph-Experiment/DPDK-FUSE/multi_process/simple_mp/build/simple_mp -c 3 -n 4 --proc-type=secondary
 #  simple_mp > send hello_secondary
-
-
 
 ./mp_server/build/mp_server -c 6 -n 4 -- -p 3 -n 2
 ./mp_client/build/mp_client -c 8 -n 4 --proc-type=auto -- -n 0
 ./mp_client/build/mp_client -c 10 -n 4 --proc-type=auto -- -n 1
 
 
+elif ["$1" = "dpdk_server_make"]
+
+cd /home/sungho/Ceph-Experiment/DPDK-FUSE/dpdk-server/
+make
+./build/dpdk-server
 
 
 fi
