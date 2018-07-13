a8ef9bde27322240cd767ef4824f439de9696e8e
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-12 20:28:32 -0700

    add

--- DPDK-FUSE/version.sh
+++ DPDK-FUSE/version.sh
@@ -13,19 +13,19 @@ elif [ "$1" = "multiprocess_tutorial" ]
 then
 /home/sungho/Ceph-Experiment/DPDK-FUSE/multi_process/simple_mp/build/simple_mp -c 3 -n 4 --proc-type=primary
 /home/sungho/Ceph-Experiment/DPDK-FUSE/multi_process/simple_mp/build/simple_mp -c 3 -n 4 --proc-type=secondary
-#  simple_mp > send hello_secondary
-#
-# ./mp_server/build/mp_server -c 6 -n 4 -- -p 3 -n 2
-# ./mp_client/build/mp_client -c 8 -n 4 --proc-type=auto -- -n 0
-# ./mp_client/build/mp_client -c 10 -n 4 --proc-type=auto -- -n 1
-#
-#
-# elif ["$1" = "dpdk_server_make" ]
-# then
-# cd /home/sungho/Ceph-Experiment/DPDK-FUSE/dpdk-server/
-# make
-# ./build/dpdk-server
-#
+ # simple_mp > send hello_secondary
+
+./mp_server/build/mp_server -c 6 -n 4 -- -p 3 -n 2
+./mp_client/build/mp_client -c 8 -n 4 --proc-type=auto -- -n 0
+./mp_client/build/mp_client -c 10 -n 4 --proc-type=auto -- -n 1
+
+
+elif [ "$1" = "dpdk_server_make" ]
+then
+cd /home/sungho/Ceph-Experiment/DPDK-FUSE/dpdk-server/
+make
+./build/dpdk-server
+
 
 else
 echo "no argument"
