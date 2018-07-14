cae1796316ad9baba2093b02b432d9d0a78b8497
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-13 21:06:53 -0700

    add

--- DPDK-FUSE/dpdk-server/main.c
+++ DPDK-FUSE/dpdk-server/main.c
@@ -149,50 +149,50 @@ struct l2fwd_port_statistics port_statistics[RTE_MAX_ETHPORTS];
 static uint64_t timer_period = 10; /* default period is 10 seconds */
 
 /* Print out statistics on packets dropped */
-static void
-print_stats(void)
-{
-	uint64_t total_packets_dropped, total_packets_tx, total_packets_rx;
-	unsigned portid;
-
-	total_packets_dropped = 0;
-	total_packets_tx = 0;
-	total_packets_rx = 0;
-
-	const char clr[] = { 27, '[', '2', 'J', '\0' };
-	const char topLeft[] = { 27, '[', '1', ';', '1', 'H','\0' };
-
-	/* Clear screen and move to top left */
-	printf("%s%s", clr, topLeft);
-
-	printf("\nPort statistics ====================================");
-
-	for (portid = 0; portid < RTE_MAX_ETHPORTS; portid++) {
-		/* skip disabled ports */
-		if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
-			continue;
-		printf("\nStatistics for port %u ------------------------------"
-			   "\nPackets sent: %24"PRIu64
-					   "\nPackets received: %20"PRIu64
-							   "\nPackets dropped: %21"PRIu64,
-			   portid,
-			   port_statistics[portid].tx,
-			   port_statistics[portid].rx,
-			   port_statistics[portid].dropped);
-
-		total_packets_dropped += port_statistics[portid].dropped;
-		total_packets_tx += port_statistics[portid].tx;
-		total_packets_rx += port_statistics[portid].rx;
-	}
-	printf("\nAggregate statistics ==============================="
-		   "\nTotal packets sent: %18"PRIu64
-				   "\nTotal packets received: %14"PRIu64
-						   "\nTotal packets dropped: %15"PRIu64,
-		   total_packets_tx,
-		   total_packets_rx,
-		   total_packets_dropped);
-	printf("\n====================================================\n");
-}
+//static void
+//print_stats(void)
+//{
+//	uint64_t total_packets_dropped, total_packets_tx, total_packets_rx;
+//	unsigned portid;
+//
+//	total_packets_dropped = 0;
+//	total_packets_tx = 0;
+//	total_packets_rx = 0;
+//
+//	const char clr[] = { 27, '[', '2', 'J', '\0' };
+//	const char topLeft[] = { 27, '[', '1', ';', '1', 'H','\0' };
+//
+//	/* Clear screen and move to top left */
+//	printf("%s%s", clr, topLeft);
+//
+//	printf("\nPort statistics ====================================");
+//
+//	for (portid = 0; portid < RTE_MAX_ETHPORTS; portid++) {
+//		/* skip disabled ports */
+//		if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
+//			continue;
+//		printf("\nStatistics for port %u ------------------------------"
+//			   "\nPackets sent: %24"PRIu64
+//					   "\nPackets received: %20"PRIu64
+//							   "\nPackets dropped: %21"PRIu64,
+//			   portid,
+//			   port_statistics[portid].tx,
+//			   port_statistics[portid].rx,
+//			   port_statistics[portid].dropped);
+//
+//		total_packets_dropped += port_statistics[portid].dropped;
+//		total_packets_tx += port_statistics[portid].tx;
+//		total_packets_rx += port_statistics[portid].rx;
+//	}
+//	printf("\nAggregate statistics ==============================="
+//		   "\nTotal packets sent: %18"PRIu64
+//				   "\nTotal packets received: %14"PRIu64
+//						   "\nTotal packets dropped: %15"PRIu64,
+//		   total_packets_tx,
+//		   total_packets_rx,
+//		   total_packets_dropped);
+//	printf("\n====================================================\n");
+//}
 
 static void
 l2fwd_mac_updating(struct rte_mbuf *m, unsigned dest_portid)
@@ -233,7 +233,6 @@ l2fwd_main_loop(void)
 {
 	struct rte_mbuf *pkts_burst[MAX_PKT_BURST];
 	struct rte_mbuf *m;
-	struct rte_mbuf *test_m;
 
 	int sent;
 	unsigned lcore_id;
