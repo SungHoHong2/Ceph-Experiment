41203df5e3acab85b273fcdae876cd11cd194ef5
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-13 20:13:22 -0700

    add

--- DPDK-FUSE/dpdk-client/main.c
+++ DPDK-FUSE/dpdk-client/main.c
@@ -193,6 +193,7 @@ l2fwd_simple_forward(struct rte_mbuf *m, unsigned portid)
 	_m = rte_pktmbuf_append(m, 1024);
 	int pkt_len = rte_pktmbuf_pkt_len(m);
 	rte_prefetch0(rte_pktmbuf_mtod(m, void *));
+	memset(_m, '*', rte_pktmbuf_pkt_len(m));
 
 	if (mac_updating)
 		l2fwd_mac_updating(m, dst_port);
@@ -203,7 +204,6 @@ l2fwd_simple_forward(struct rte_mbuf *m, unsigned portid)
 		port_statistics[dst_port].tx += sent;
 
 	// printf("pkt_len after: %d\n",pkt_len);
-     memset(_m, '*', rte_pktmbuf_pkt_len(m));
 	 rte_pktmbuf_dump(stdout, m, 1024);
 
 
