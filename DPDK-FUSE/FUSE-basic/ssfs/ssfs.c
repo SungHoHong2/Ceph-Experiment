619fe6ba3984969bd5d22696fc88f8ec92016228
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-14 19:45:53 -0700

    add

--- DPDK-FUSE/FUSE-basic/ssfs/ssfs.c
+++ DPDK-FUSE/FUSE-basic/ssfs/ssfs.c
@@ -358,69 +358,11 @@ l2fwd_parse_args(int argc, char **argv)
     char **argvopt;
     int option_index;
     char *prgname = argv[0];
-
     argvopt = argv;
     l2fwd_enabled_port_mask = l2fwd_parse_portmask("0x1");
     l2fwd_rx_queue_per_lcore = l2fwd_parse_nqueue("8");
     timer_secs = l2fwd_parse_timer_period("1");
-
-//
-//    while ((opt = getopt_long(argc, argvopt, short_options,
-//                              lgopts, &option_index)) != EOF) {
-//
-//
-//        switch (opt) {
-//            /* portmask */
-//            case 'p':
-//                printf("optarg p: %s\n",optarg);
-//                l2fwd_enabled_port_mask = l2fwd_parse_portmask("0x1");
-//                if (l2fwd_enabled_port_mask == 0) {
-//                    printf("invalid portmask\n");
-//                    l2fwd_usage(prgname);
-//                    return -1;
-//                }
-//                break;
-//
-//                /* nqueue */
-//            case 'q':
-//                printf("optarg q: %s\n",optarg);
-//                l2fwd_rx_queue_per_lcore = l2fwd_parse_nqueue("8");
-//                if (l2fwd_rx_queue_per_lcore == 0) {
-//                    printf("invalid queue number\n");
-//                    l2fwd_usage(prgname);
-//                    return -1;
-//                }
-//                break;
-//
-//                /* timer period */
-//            case 'T':
-//                printf("optarg T: %s\n",optarg);
-//                timer_secs = l2fwd_parse_timer_period("1");
-//                if (timer_secs < 0) {
-//                    printf("invalid timer period\n");
-//                    l2fwd_usage(prgname);
-//                    return -1;
-//                }
-//                timer_period = timer_secs;
-//                break;
-//
-//                /* long options */
-//            case 0:
-//                break;
-//
-//            default:
-//                l2fwd_usage(prgname);
-//                return -1;
-//        }
-//    }
-//
-//    if (optind >= 0)
-//        argv[optind-1] = prgname;
-//
-//    ret = optind-1;
-//    optind = 1; /* reset getopt lib */
     ret = 6;
-    printf("ret: %d\n", ret);
     return ret;
 }
 
