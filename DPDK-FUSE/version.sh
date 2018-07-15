b26ab1bfded372310be0cd0c7483b79b3b7ee405
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-14 17:01:56 -0700

    add

--- DPDK-FUSE/version.sh
+++ DPDK-FUSE/version.sh
@@ -36,7 +36,7 @@ elif [ "$1" = "ssfs_make" ]
 then
   cd /home/sungho/Ceph-Experiment/DPDK-FUSE/FUSE-basic/ssfs
   make
-  ./build/ssfs  -f /mnt/ssd_cache/test
+  ./build/ssfs -f /mnt/ssd_cache/test
 
 elif [ "$1" = "ssfs_client" ]
 then
