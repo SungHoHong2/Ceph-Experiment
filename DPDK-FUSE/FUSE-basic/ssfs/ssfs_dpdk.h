0543bd20285968f4374e5db078d85a60e1b808aa
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-15 20:21:34 -0700

    add

--- DPDK-FUSE/FUSE-basic/ssfs/ssfs_dpdk.h
+++ DPDK-FUSE/FUSE-basic/ssfs/ssfs_dpdk.h
@@ -302,3 +302,245 @@ check_all_ports_link_status(uint8_t port_num, uint32_t port_mask)
         }
     }
 }
+
+
+static void
+signal_handler(int signum)
+{
+    if (signum == SIGINT || signum == SIGTERM) {
+        printf("\n\nSignal %d received, preparing to exit...\n",
+               signum);
+        force_quit = true;
+    }
+}
+
+
+struct thread_data
+{
+    int c;
+    char **v;
+};
+
+void *dpdk_msg_launch(void *threadarg) {
+
+    printf("DPDK BEGIN\n");
+
+    struct thread_data *my_data;
+    my_data = (struct thread_data *) threadarg;
+
+    int argc = my_data->c;
+    char **argv = my_data->v;
+
+    sleep(5);
+
+    struct lcore_queue_conf *qconf;
+    struct rte_eth_dev_info dev_info;
+    int ret;
+    uint8_t nb_ports;
+    uint8_t nb_ports_available;
+    uint8_t portid, last_port;
+    unsigned lcore_id, rx_lcore_id;
+    unsigned nb_ports_in_mask = 0;
+
+
+    int dpdk_argc = 12;
+    char** dpdk_argv;
+    dpdk_argv = malloc(dpdk_argc * sizeof(char*));
+    for (size_t i = 0; i < dpdk_argc; i += 1)
+        dpdk_argv[i] = malloc(255 * sizeof(char));
+
+    dpdk_argv[0]="./sfss";
+    dpdk_argv[1]="-c";
+    dpdk_argv[2]="0x2";
+    dpdk_argv[3]="-n";
+    dpdk_argv[4]="4";
+    dpdk_argv[5]="--";
+    dpdk_argv[6]="-q";
+    dpdk_argv[7]="8";
+    dpdk_argv[8]="-p";
+    dpdk_argv[9]="0x1";
+    dpdk_argv[10]="-T";
+    dpdk_argv[11]="1";
+
+    /* init EAL */
+    ret = rte_eal_init(dpdk_argc, dpdk_argv);
+    if (ret < 0)
+        rte_exit(EXIT_FAILURE, "Invalid EAL arguments\n");
+    argc -= ret;
+    argv += ret;
+
+    force_quit = false;
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
+    /* parse application arguments (after the EAL ones) */
+    ret = l2fwd_parse_args(argc, argv);
+    if (ret < 0)
+        rte_exit(EXIT_FAILURE, "Invalid L2FWD arguments\n");
+
+    printf("MAC updating %s\n", mac_updating ? "enabled" : "disabled");
+
+
+
+    /* create the mbuf pool */
+    l2fwd_pktmbuf_pool = rte_pktmbuf_pool_create("mbuf_pool", NB_MBUF,
+                                                 MEMPOOL_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE,
+                                                 rte_socket_id());
+    if (l2fwd_pktmbuf_pool == NULL)
+        rte_exit(EXIT_FAILURE, "Cannot init mbuf pool\n");
+
+    nb_ports = rte_eth_dev_count();
+    if (nb_ports == 0)
+        rte_exit(EXIT_FAILURE, "No Ethernet ports - bye\n");
+
+    /* reset l2fwd_dst_ports */
+    for (portid = 0; portid < RTE_MAX_ETHPORTS; portid++)
+        l2fwd_dst_ports[portid] = 0;
+    last_port = 0;
+
+    /*
+     * Each logical core is assigned a dedicated TX queue on each port.
+     */
+    for (portid = 0; portid < nb_ports; portid++) {
+        /* skip ports that are not enabled */
+        if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
+            continue;
+
+        if (nb_ports_in_mask % 2) {
+            l2fwd_dst_ports[portid] = last_port;
+            l2fwd_dst_ports[last_port] = portid;
+        }
+        else
+            last_port = portid;
+
+        nb_ports_in_mask++;
+
+        rte_eth_dev_info_get(portid, &dev_info);
+    }
+    if (nb_ports_in_mask % 2) {
+        printf("Notice: odd number of ports in portmask.\n");
+        l2fwd_dst_ports[last_port] = last_port;
+    }
+
+    rx_lcore_id = 0;
+    qconf = NULL;
+
+    /* Initialize the port/queue configuration of each logical core */
+    for (portid = 0; portid < nb_ports; portid++) {
+        /* skip ports that are not enabled */
+        if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
+            continue;
+
+        /* get the lcore_id for this port */
+        while (rte_lcore_is_enabled(rx_lcore_id) == 0 ||
+               lcore_queue_conf[rx_lcore_id].n_rx_port ==
+               l2fwd_rx_queue_per_lcore) {
+            rx_lcore_id++;
+            if (rx_lcore_id >= RTE_MAX_LCORE)
+                rte_exit(EXIT_FAILURE, "Not enough cores\n");
+        }
+
+        if (qconf != &lcore_queue_conf[rx_lcore_id])
+            /* Assigned a new logical core in the loop above. */
+            qconf = &lcore_queue_conf[rx_lcore_id];
+
+        qconf->rx_port_list[qconf->n_rx_port] = portid;
+        qconf->n_rx_port++;
+        printf("Lcore %u: RX port %u\n", rx_lcore_id, (unsigned) portid);
+    }
+
+    nb_ports_available = nb_ports;
+
+
+
+/* Initialise each port */
+    for (portid = 0; portid < nb_ports; portid++) {
+        /* skip ports that are not enabled */
+        if ((l2fwd_enabled_port_mask & (1 << portid)) == 0) {
+            printf("Skipping disabled port %u\n", (unsigned) portid);
+            nb_ports_available--;
+            continue;
+        }
+
+        /* init port */
+        printf("Initializing port %u... ", (unsigned) portid);
+        fflush(stdout);
+        ret = rte_eth_dev_configure(portid, 1, 1, &port_conf);
+        if (ret < 0)
+            rte_exit(EXIT_FAILURE, "Cannot configure device: err=%d, port=%u\n",
+                     ret, (unsigned) portid);
+
+        rte_eth_macaddr_get(portid,&l2fwd_ports_eth_addr[portid]);
+
+        /* init one RX queue */
+        fflush(stdout);
+        ret = rte_eth_rx_queue_setup(portid, 0, nb_rxd,
+                                     rte_eth_dev_socket_id(portid),
+                                     NULL,
+                                     l2fwd_pktmbuf_pool);
+        if (ret < 0)
+            rte_exit(EXIT_FAILURE, "rte_eth_rx_queue_setup:err=%d, port=%u\n",
+                     ret, (unsigned) portid);
+
+        /* init one TX queue on each port */
+        fflush(stdout);
+        ret = rte_eth_tx_queue_setup(portid, 0, nb_txd,
+                                     rte_eth_dev_socket_id(portid),
+                                     NULL);
+        if (ret < 0)
+            rte_exit(EXIT_FAILURE, "rte_eth_tx_queue_setup:err=%d, port=%u\n",
+                     ret, (unsigned) portid);
+
+        /* Initialize TX buffers */
+        tx_buffer[portid] = rte_zmalloc_socket("tx_buffer",
+                                               RTE_ETH_TX_BUFFER_SIZE(MAX_PKT_BURST), 0,
+                                               rte_eth_dev_socket_id(portid));
+        if (tx_buffer[portid] == NULL)
+            rte_exit(EXIT_FAILURE, "Cannot allocate buffer for tx on port %u\n",
+                     (unsigned) portid);
+
+        rte_eth_tx_buffer_init(tx_buffer[portid], MAX_PKT_BURST);
+
+
+        /* Start device */
+        ret = rte_eth_dev_start(portid);
+        if (ret < 0)
+            rte_exit(EXIT_FAILURE, "rte_eth_dev_start:err=%d, port=%u\n",
+                     ret, (unsigned) portid);
+
+        printf("done: \n");
+
+        rte_eth_promiscuous_enable(portid);
+
+        printf("Port %u, MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n\n",
+               (unsigned) portid,
+               l2fwd_ports_eth_addr[portid].addr_bytes[0],
+               l2fwd_ports_eth_addr[portid].addr_bytes[1],
+               l2fwd_ports_eth_addr[portid].addr_bytes[2],
+               l2fwd_ports_eth_addr[portid].addr_bytes[3],
+               l2fwd_ports_eth_addr[portid].addr_bytes[4],
+               l2fwd_ports_eth_addr[portid].addr_bytes[5]);
+
+    }
+
+    if (!nb_ports_available) {
+        rte_exit(EXIT_FAILURE,
+                 "All available ports are disabled. Please set portmask.\n");
+    }
+
+    check_all_ports_link_status(nb_ports, l2fwd_enabled_port_mask);
+
+
+    ret = 0;
+    /* launch per-lcore init on every lcore */
+    rte_eal_mp_remote_launch(l2fwd_launch_one_lcore, NULL, CALL_MASTER);
+    RTE_LCORE_FOREACH_SLAVE(lcore_id) {
+        if (rte_eal_wait_lcore(lcore_id) < 0) {
+            ret = -1;
+            break;
+        }
+    }
+
+    printf("DPDK END\n");
+}
+
