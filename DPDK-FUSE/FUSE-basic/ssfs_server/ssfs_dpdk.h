0a632cb2b6de6577acdc77e11af15f68e9bd565f
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-16 20:34:25 -0700

    add

--- DPDK-FUSE/FUSE-basic/ssfs_server/ssfs_dpdk.h
+++ DPDK-FUSE/FUSE-basic/ssfs_server/ssfs_dpdk.h
@@ -121,7 +121,7 @@ dpdk_packet_hexdump(FILE *f, const char * title, const void * buf, unsigned int 
     ofs = start;
     data+=ofs;
     struct message *msg = (struct message *) data;
-    fprintf(f,"recv msg in DPDK: %s\n", msg->data);
+    fprintf(f,"recv msg in DPDK: %s len:%d\n", msg->datam, len);
 
     pthread_mutex_lock(&rx_lock);
     struct fuse_message * e = NULL;
