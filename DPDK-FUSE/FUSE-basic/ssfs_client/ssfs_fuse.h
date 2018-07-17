e5c8167600a7f6f4222a065a7c17013e8a10e1c2
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-16 21:05:05 -0700

    add

--- DPDK-FUSE/FUSE-basic/ssfs_client/ssfs_fuse.h
+++ DPDK-FUSE/FUSE-basic/ssfs_client/ssfs_fuse.h
@@ -62,7 +62,7 @@ static int do_read( const char *path, char *buffer, size_t size, off_t offset, s
         pthread_mutex_unlock(&tx_lock);
 
 
-        pthread_mutex_lock(&rx_lock);
+//        pthread_mutex_lock(&rx_lock);
 //        while(TAILQ_EMPTY(&fuse_rx_queue));
 //
 //        if(!TAILQ_EMPTY(&fuse_rx_queue)) {
