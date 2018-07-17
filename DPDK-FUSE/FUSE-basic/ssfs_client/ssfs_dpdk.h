0196429f80bcd53ea509ea5ced871761729b17f5
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-17 10:59:17 -0700

    add

--- DPDK-FUSE/FUSE-basic/ssfs_client/ssfs_dpdk.h
+++ DPDK-FUSE/FUSE-basic/ssfs_client/ssfs_dpdk.h
@@ -215,11 +215,13 @@ l2fwd_main_loop(void)
                 if(header_length>0){
                     // printf("rte_mbuf_packet_length: %d\n", rte_mbuf_packet_length);  // lenght of the offset: 456
                     // printf("header_length: %d\n", header_length);  // lenght of the offset: 456
-                    dpdk_pktmbuf_dump(stdout, m, 1024, header_length);
+                    rte_pktmbuf_dump(stdout, m, 1024, header_length);
+                    // dpdk_pktmbuf_dump(stdout, m, 1024, header_length);
                 }
                 //CHARA END
                 rte_prefetch0(rte_pktmbuf_mtod(m, void *));
                 l2fwd_simple_forward(m, portid);
+                // rte_pktmbuf_free(m);
             }
 
 
