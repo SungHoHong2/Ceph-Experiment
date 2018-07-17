27a8b6e241a2061927e65a86077332cdadde7fb9
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-17 16:11:35 -0700

    add

--- DPDK-FUSE/FUSE-basic/ssfs_benchmark/main.c
+++ DPDK-FUSE/FUSE-basic/ssfs_benchmark/main.c
@@ -13,6 +13,7 @@ int main (int arc, char * argv [])
     char read_file[1024];
     FILE *file;
     for(c=0; c<NUMBER_OF_REQUEST; c++){
+        sleep(1);
         file = fopen("/mnt/ssd_cache/test/client", "r");
         if (file) {
             fread(read_file, sizeof(char), 1024, file);
