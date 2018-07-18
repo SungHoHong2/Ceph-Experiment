89526f4905afd9199f94adda45f10c42f680d303
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-17 18:02:17 -0700

    add

--- DPDK-FUSE/FUSE-basic/posix_server/ssfs_posix.h
+++ DPDK-FUSE/FUSE-basic/posix_server/ssfs_posix.h
@@ -94,7 +94,7 @@ void *tcp_msg_launch(){
         perror("accept");
     }
 
-    fcntl(new_fd, F_SETFL, O_NONBLOCK);
+//    fcntl(new_fd, F_SETFL, O_NONBLOCK);
 
     while(1) {  // main accept() loop
         inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
