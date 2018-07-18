d304a4c4a3963df83c16df85fae4b6308c9e606e
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-17 17:30:32 -0700

    add

--- DPDK-FUSE/FUSE-basic/posix_server/ssfs.c
+++ DPDK-FUSE/FUSE-basic/posix_server/ssfs.c
@@ -27,29 +27,7 @@
 #include <signal.h>
 #include <sys/fcntl.h>
 
-#include <rte_common.h>
-#include <rte_log.h>
-#include <rte_malloc.h>
-#include <rte_memory.h>
-#include <rte_memcpy.h>
-#include <rte_memzone.h>
-#include <rte_eal.h>
-#include <rte_per_lcore.h>
-#include <rte_launch.h>
-#include <rte_atomic.h>
-#include <rte_cycles.h>
-#include <rte_prefetch.h>
-#include <rte_lcore.h>
-#include <rte_per_lcore.h>
-#include <rte_branch_prediction.h>
-#include <rte_interrupts.h>
-#include <rte_pci.h>
-#include <rte_random.h>
-#include <rte_debug.h>
-#include <rte_ether.h>
-#include <rte_ethdev.h>
-#include <rte_mempool.h>
-#include <rte_mbuf.h>
+
 #include <sys/queue.h>
 #include "ssfs_common.h"
 #include "ssfs_posix.h"
