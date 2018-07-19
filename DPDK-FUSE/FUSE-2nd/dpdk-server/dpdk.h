481ce3e256b8165beec858f42529152f5290a28c
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-19 14:12:02 -0700

    add

--- DPDK-FUSE/FUSE-2nd/dpdk-server/dpdk.h
+++ DPDK-FUSE/FUSE-2nd/dpdk-server/dpdk.h
@@ -7,7 +7,10 @@ static int mac_updating = 1;
 
 #define NB_MBUF   8192
 
-#define MAX_PKT_BURST 32
+// #define MAX_PKT_BURST 32
+#define MAX_PKT_BURST 1
+
+
 #define BURST_TX_DRAIN_US 100 /* TX drain every ~100us */
 #define MEMPOOL_CACHE_SIZE 256
 
--- DPDK-FUSE/FUSE-2nd/dpdk_client/ssfs_dpdk.h
+++ DPDK-FUSE/FUSE-2nd/dpdk_client/ssfs_dpdk.h
@@ -8,7 +8,9 @@ static int mac_updating = 1;
 
 #define NB_MBUF   8192
 
-#define MAX_PKT_BURST 32
+// #define MAX_PKT_BURST 32
+#define MAX_PKT_BURST 1
+
 #define BURST_TX_DRAIN_US 100 /* TX drain every ~100us */
 #define MEMPOOL_CACHE_SIZE 256
 static volatile int timerz = 100;
