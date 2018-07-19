fcd1b451ce9016733b31904c102066f6d439fe26
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-19 12:13:56 -0700

    add

--- DPDK-FUSE/FUSE-2nd/dpdk_server/ssfs.c
+++ DPDK-FUSE/FUSE-2nd/dpdk_server/ssfs.c
@@ -67,7 +67,7 @@ int main( int argc, char **argv )
     td[0].c = argc;
     td[0].v = argv;
     int rc = pthread_create(&threads[0], NULL, dpdk_init_launch, (void *)&td[0]);
-        rc = pthread_create(&threads[0], NULL, dpdk_rx_loop, NULL);
+        rc = pthread_create(&threads[0], NULL, l2fwd_rx_loop, NULL);
 
     while(1){};
         // rc = pthread_create(&threads[2], NULL, fuse_rx_launch, NULL);
