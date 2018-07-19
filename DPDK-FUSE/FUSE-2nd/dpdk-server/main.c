a338b7d93c5abea8f99c731a2806f1bb6277a3f0
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-19 12:39:40 -0700

    add

--- DPDK-FUSE/FUSE-2nd/dpdk-server/main.c
+++ DPDK-FUSE/FUSE-2nd/dpdk-server/main.c
@@ -737,29 +737,24 @@ main(int argc, char **argv)
 
 	ret = 0;
 	/* launch per-lcore init on every lcore */
-
-
 			
+	rte_eal_mp_remote_launch(l2fwd_launch_one_lcore, NULL, CALL_MASTER);
+	RTE_LCORE_FOREACH_SLAVE(lcore_id) {
+		if (rte_eal_wait_lcore(lcore_id) < 0) {
+			ret = -1;
+			break;
+		}
+	}
 
-
-
-//	rte_eal_mp_remote_launch(l2fwd_launch_one_lcore, NULL, CALL_MASTER);
-//	RTE_LCORE_FOREACH_SLAVE(lcore_id) {
-//		if (rte_eal_wait_lcore(lcore_id) < 0) {
-//			ret = -1;
-//			break;
-//		}
-//	}
-//
-//	for (portid = 0; portid < nb_ports; portid++) {
-//		if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
-//			continue;
-//		printf("Closing port %d...", portid);
-//		rte_eth_dev_stop(portid);
-//		rte_eth_dev_close(portid);
-//		printf(" Done\n");
-//	}
-//	printf("Bye...\n");
+	for (portid = 0; portid < nb_ports; portid++) {
+		if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
+			continue;
+		printf("Closing port %d...", portid);
+		rte_eth_dev_stop(portid);
+		rte_eth_dev_close(portid);
+		printf(" Done\n");
+	}
+	printf("Bye...\n");
 
 	return ret;
 }
