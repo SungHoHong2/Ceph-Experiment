d659f688fe5a768e75772d270f4103e008620d57
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-19 14:36:06 -0700

    add

--- DPDK-FUSE/FUSE-2nd/dpdk_client/ssfs_fuse.h
+++ DPDK-FUSE/FUSE-2nd/dpdk_client/ssfs_fuse.h
@@ -105,7 +105,7 @@ void *fuse_rx_launch() {
             av->interval = av->end_time - av->start_time;
             TAILQ_INSERT_TAIL(&avg_queue, av, nodes);
 
-            if(total_requests<TOTAL_TEST_REQ){
+            if(total_requests > TOTAL_TEST_REQ){
                 avg_results();
             }
 
