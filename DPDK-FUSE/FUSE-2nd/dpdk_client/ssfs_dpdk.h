4f6a0faf10322e05c7c2ec57b8b7dd44b78fb5b8
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-19 17:24:17 -0700

    add

--- DPDK-FUSE/FUSE-2nd/dpdk_client/ssfs_dpdk.h
+++ DPDK-FUSE/FUSE-2nd/dpdk_client/ssfs_dpdk.h
@@ -203,7 +203,7 @@ void
                 int header_length = rte_mbuf_packet_length - 1024;
 
                 if (rte_mbuf_packet_length == 1024) {
-                     rte_pktmbuf_dump(stdout, m, 1024);
+                    // rte_pktmbuf_dump(stdout, m, 1024);
                     dpdk_pktmbuf_dump(stdout, m, 1024, sizeof(struct ether_hdr));
                 }
                 //CHARA END
--- DPDK-FUSE/FUSE-2nd/posix_client/ssfs_fuse.h
+++ DPDK-FUSE/FUSE-2nd/posix_client/ssfs_fuse.h
@@ -99,7 +99,7 @@ void *fuse_rx_launch() {
             e = TAILQ_FIRST(&fuse_rx_queue);
             total_requests++;
 
-            printf("recv msg in FUSE: %s :: %d\n", e->data, total_requests);
+            printf("recv msg in FUSE: %ld :: %d\n", strlen(e->data), total_requests);
             av->end_time = getTimeStamp();
             av->interval = av->end_time - av->start_time;
             // printf("%ld\n",av->interval);
