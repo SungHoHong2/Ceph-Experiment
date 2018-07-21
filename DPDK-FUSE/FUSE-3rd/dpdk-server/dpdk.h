bc99a0b64c0a0a155c891102532b523c0bf2f66c
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-20 22:44:16 -0700

    add

--- DPDK-FUSE/FUSE-3rd/dpdk-server/dpdk.h
+++ DPDK-FUSE/FUSE-3rd/dpdk-server/dpdk.h
@@ -116,11 +116,12 @@ dpdk_packet_hexdump(FILE *f, const char * title, const void * buf, unsigned int 
     struct fuse_message *e = NULL;
     struct message *msg = (struct message *) data;
 
+    fprintf(f, "recv msg in DPDK: %s\n",msg->data);
     if(strlen(msg->data)>=24 && strcmp(msg->data, "Hello World From CLIENT!\n")==0) {
 
         pthread_mutex_lock(&rx_lock);
         e = malloc(sizeof(struct fuse_message));
-        // fprintf(f, "recv msg in DPDK: %s\n",msg->data);
+        fprintf(f, "recv msg in DPDK: %s\n",msg->data);
         strcpy(e->data, msg->data);
         TAILQ_INSERT_TAIL(&fuse_rx_queue, e, nodes);
         fflush(f);
