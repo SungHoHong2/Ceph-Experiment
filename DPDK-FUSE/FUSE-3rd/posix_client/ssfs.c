ac8eeae6f36d12b74f10d34b055461c26f605a3a
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-21 19:09:17 -0700

    add

---
+++ DPDK-FUSE/FUSE-3rd/posix_client/ssfs.c
@@ -0,0 +1,64 @@
+#define FUSE_USE_VERSION 30
+
+#include <fuse.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <sys/queue.h>
+#include <netinet/in.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <errno.h>
+#include <getopt.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/fcntl.h>
+#include <pthread.h>
+
+#include "ssfs_common.h"
+#include "ssfs_posix.h"
+#include "ssfs_fuse.h"
+
+
+int main( int argc, char **argv )
+{
+    TAILQ_INIT(&avg_queue);
+    TAILQ_INIT(&fuse_tx_queue);
+    TAILQ_INIT(&fuse_rx_queue);
+
+    if (pthread_mutex_init(&tx_lock, NULL) != 0) {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
+
+    if (pthread_mutex_init(&rx_lock, NULL) != 0) {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
+
+    printf("FUSE-POSIX BEGIN\n");
+    pthread_t threads[3];
+    int rc = pthread_create(&threads[0], NULL, tcp_send_launch, NULL);
+    rc = pthread_create(&threads[1], NULL, tcp_recv_launch, NULL);
+    rc = pthread_create(&threads[2], NULL, fuse_rx_launch, NULL);
+
+
+    while(1){};
+//    printf("FUSE BEGIN\n");
+//    fuse_main( argc, argv, &operations, NULL );
+//    printf("FUSE END\n");
+    return 0;
+}
---
+++ DPDK-FUSE/FUSE-3rd/posix_client/ssfs_fuse.h
@@ -0,0 +1,128 @@
+#define TOTAL_TEST_REQ 10
+
+//static int do_getattr( const char *path, struct stat *st )
+//{
+//    // printf( "[getattr] Called\n" );
+//    // printf( "\tAttributes of %s requested\n", path );
+//
+//    st->st_uid = getuid(); // The owner of the file/directory is the user who mounted the filesystem
+//    st->st_gid = getgid(); // The group of the file/directory is the same as the group of the user who mounted the filesystem
+//    st->st_atime = time( NULL ); // The last "a"ccess of the file/directory is right now
+//    st->st_mtime = time( NULL ); // The last "m"odification of the file/directory is right now
+//
+//    if ( strcmp( path, "/" ) == 0 )
+//    {
+//        st->st_mode = S_IFDIR | 0755;
+//        st->st_nlink = 2; // Why "two" hardlinks instead of "one"? The answer is here: http://unix.stackexchange.com/a/101536
+//    }
+//    else
+//    {
+//        st->st_mode = S_IFREG | 0644;
+//        st->st_nlink = 1;
+//        st->st_size = 1024;
+//    }
+//
+//    return 0;
+//}
+//
+//static int do_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi )
+//{
+//    // printf( "--> Getting The List of Files of %s\n", path );
+//
+//    filler( buffer, ".", NULL, 0 ); // Current Directory
+//    filler( buffer, "..", NULL, 0 ); // Parent Directory
+//
+//    if ( strcmp( path, "/" ) == 0 ) // If the user is trying to show the files/directories of the root directory show the following
+//    {
+//        filler( buffer, "client", NULL, 0 );
+//        filler( buffer, "server", NULL, 0 );
+//    }
+//
+//    return 0;
+//}
+//
+//struct avg_node *av = NULL;
+//int s = 0;
+//static int do_read( const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi )
+//{
+//    // printf( "--> Trying to read %s, %lu, %lu\n", path, offset, size );
+//
+//    char client[] = "Hello World From CLIENT!\n";
+//    char server[] = "Hello World From SERVER!\n";
+//    char *selectedText = NULL;
+//    struct fuse_message *e = NULL;
+//
+//    // ... //
+//
+//    if ( strcmp( path, "/client" ) == 0 ) {
+//
+//        sleep(0);
+//        selectedText = client;
+//        pthread_mutex_lock(&tx_lock);
+//        e = malloc(sizeof(struct fuse_message));
+//        strcpy(e->data, selectedText);
+//        TAILQ_INSERT_TAIL(&fuse_tx_queue, e, nodes);
+//        // printf("send msg in FUSE: %s %d\n", e->data, s++);
+//
+//        av = malloc(sizeof(struct avg_node));
+//        av->start_time = getTimeStamp();
+//
+//
+//        pthread_mutex_unlock(&tx_lock);
+//
+//
+//    } else if ( strcmp( path, "/server" ) == 0 )
+//        selectedText = server;
+//    else
+//        return -1;
+//
+//    memcpy( buffer, selectedText + offset, size );
+//    return strlen( selectedText ) - offset;
+//}
+//
+//static struct fuse_operations operations = {
+//        .getattr	= do_getattr,
+//        .readdir	= do_readdir,
+//        .read		= do_read,
+//};
+
+
+void *fuse_rx_launch() {
+    printf("FUSE-RX BEGIN\n");
+    struct fuse_message * e = NULL;
+    struct fuse_message * txe = NULL;
+    char *buffer = NULL;
+    int rtn;
+
+    while(1) {
+        int c;
+        // printf("fuse_rx_queue checking : %d\n", TAILQ_EMPTY(&fuse_rx_queue));
+        pthread_mutex_lock(&rx_lock);
+        if(!TAILQ_EMPTY(&fuse_rx_queue)) {
+            e = TAILQ_FIRST(&fuse_rx_queue);
+            TAILQ_REMOVE(&fuse_rx_queue, e, nodes);
+
+
+            // printf("recv msg in FUSE: %ld :: %d\n", strlen(e->data), total_requests);
+
+            av->end_time = getTimeStamp();
+            av->interval = av->end_time - av->start_time;
+            // printf("%ld\n",av->interval);
+
+            printf("recv msg in FUSE: %ld :: %ld :: %d\n", strlen(e->data), av->interval, total_requests);
+
+
+            total_requests++;
+//            TAILQ_INSERT_TAIL(&avg_queue, av, nodes);
+
+//            if(total_requests >= TOTAL_TEST_REQ){
+//                avg_results();
+//                break;
+//            }
+
+            free(e);
+            e = NULL;
+        }
+        pthread_mutex_unlock(&rx_lock);
+    }
+}
