0df641aa54d0a02db974f59eaa9d14a7b6ea032f
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-21 21:29:10 -0700

    add

--- DPDK-FUSE/FUSE-3rd/dpdk-server/dpdk.h
+++ DPDK-FUSE/FUSE-3rd/dpdk-server/dpdk.h
@@ -173,7 +173,7 @@ l2fwd_rx_loop()
                 int rte_mbuf_packet_length = rte_pktmbuf_pkt_len(m);
 
 
-                printf("CHARA: %d\n",rte_mbuf_packet_length);
+                // printf("CHARA: %d\n",rte_mbuf_packet_length);
                 if(rte_mbuf_packet_length==PKT_SIZE){
                     rte_pktmbuf_dump(stdout,m,PKT_SIZE);
                     dpdk_pktmbuf_dump(stdout, m, PKT_SIZE, sizeof(struct ether_hdr));
