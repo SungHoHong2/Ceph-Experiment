1c9a65dede3325bab804b81088f8675c501c1112
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-22 21:10:42 -0700

    add

--- DPDK-FUSE/FUSE-3rd/dpdk_client/ssfs_dpdk.h
+++ DPDK-FUSE/FUSE-3rd/dpdk_client/ssfs_dpdk.h
@@ -160,11 +160,12 @@ l2fwd_tx_loop()
 
 
 
-        if (rte_ring_dequeue(tx_ring, __msg) < 0) {
+
+        if (rte_ring_dequeue(tx_ring, &__msg) < 0) {
             // printf("Failed to recv message - message discarded\n");
         } else {
-                 // _msg = (struct message *)__msg;
-                 // printf("send msg in DPDK: %s\n",_msg->data);
+                  _msg = (struct message *)__msg;
+                  printf("send msg in DPDK: %s\n",_msg->data);
 //                dpdk_av = malloc(sizeof(struct avg_node));
 //                dpdk_av->start_time = getTimeStamp();
 //                dpdk_av->num = dpdk_requests;
