de59c4f6ed75db33d2233b29db0e53db24f94c81
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-22 21:34:30 -0700

    add

--- DPDK-FUSE/FUSE-3rd/dpdk_client/ssfs_fuse.h
+++ DPDK-FUSE/FUSE-3rd/dpdk_client/ssfs_fuse.h
@@ -51,7 +51,6 @@ static int do_read( const char *path, char *buffer, size_t size, off_t offset, s
     char server[] = "Hello World From SERVER!\n";
     char *selectedText = NULL;
     struct fuse_message *e = NULL;
-    char *buffer = NULL;
     void *msg;
     struct message *_msg;
     // ... //
