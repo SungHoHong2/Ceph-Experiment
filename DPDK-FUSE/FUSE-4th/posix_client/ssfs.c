f1a29a0cb975dd28e4ea5546843ec59d2aeb65d7
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-23 23:28:06 -0700

    add

--- DPDK-FUSE/FUSE-4th/posix_client/ssfs.c
+++ DPDK-FUSE/FUSE-4th/posix_client/ssfs.c
@@ -71,9 +71,9 @@ int main( int argc, char **argv )
     }
 
     printf("FUSE-POSIX BEGIN\n");
-    pthread_t threads[3];
+    pthread_t threads[2];
     int rc = pthread_create(&threads[0], NULL, tcp_send_launch, NULL);
-//    rc = pthread_create(&threads[1], NULL, tcp_recv_launch, NULL);
+    rc = pthread_create(&threads[1], NULL, tcp_recv_launch, NULL);
 
     printf("FUS-CLIENT BEGIN\n");
     umask(0);
