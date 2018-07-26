895e1b2bc896afe83643ae9500a3e6eaf6d04a99
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-26 16:18:58 -0700

    add

--- DPDK-FUSE/FUSE-4th/dpdk-server/dpdk.h
+++ DPDK-FUSE/FUSE-4th/dpdk-server/dpdk.h
@@ -224,11 +224,11 @@ l2fwd_rx_loop()
                 int rte_mbuf_packet_length = rte_pktmbuf_pkt_len(m);
 
                 // rte_pktmbuf_dump(stdout, m, 60);
-                l2fwd_simple_forward(m,1);
+                // l2fwd_simple_forward(m,1);
 
-                // if(rte_mbuf_packet_length==PKT_SIZE) {
-                //        dpdk_pktmbuf_dump(stdout, m, PKT_SIZE, 0);
-                // }
+                 if(rte_mbuf_packet_length==PKT_SIZE) {
+                        dpdk_pktmbuf_dump(stdout, m, PKT_SIZE, 0);
+                 }
 
             }
     }
