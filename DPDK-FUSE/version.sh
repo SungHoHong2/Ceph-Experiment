67213122d489bf0b597f9e1727f218b120f6e8b7
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-26 18:21:22 -0700

    add

--- DPDK-FUSE/version.sh
+++ DPDK-FUSE/version.sh
@@ -204,7 +204,7 @@ elif [ "$HOSTS" = "c3n25" ]
 then
 cd /data1/sungho/Ceph-Experiment/DPDK-FUSE/FUSE-4th/posix_client
 gcc -D _BSD_SOURCE ssfs.c -o ssfs -lm -D_FILE_OFFSET_BITS=64 -I/usr/local/include/fuse3 -L/usr/local/lib/x86_64-linux-gnu -lfuse3 -lpthread
-sudo -u sungho ./ssfs -f /data1/sungho/trash
+./ssfs -f /data1/sungho/trash
 fi
 
 # RUNNING THE FUSE-DPDK END
