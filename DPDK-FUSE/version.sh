c8a0459b86d29e8af8350d6f001ccaad381dd906
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-26 17:27:11 -0700

    add

--- DPDK-FUSE/version.sh
+++ DPDK-FUSE/version.sh
@@ -166,7 +166,7 @@ then
   export RTE_SDK=/data1/sungho/dpdk-stable-17.05.1
   cd /data1/sungho/Ceph-Experiment/DPDK-FUSE/FUSE-4th/dpdk-server/
   make
-  ./build/dpdk-server "$1" 
+  ./build/dpdk-server "NOFILESYSTEM"
 
 
   elif [ "$HOSTS" = "c3n25" ]
