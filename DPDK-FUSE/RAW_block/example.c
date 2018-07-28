275a4fb2a066658252ec3f14cda2bec2917ff6d5
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-27 20:10:56 -0700

    add

--- DPDK-FUSE/RAW_block/example.c
+++ DPDK-FUSE/RAW_block/example.c
@@ -22,6 +22,12 @@
 
 
 int main(){
+    // fd = open(argv[1], O_RDONLY | O_DIRECT);
+    int fd = open("/mnt/ssd_cache", O_RDWR | O_DIRECT);
+
+    
+
+
 
     printf("howdy\n");
     return 0;
--- DPDK-FUSE/version.sh
+++ DPDK-FUSE/version.sh
@@ -29,6 +29,12 @@ then
   # umount -l /data1/sungho/trash
   umount -l /mnt/ssd_cache
   sudo mount -t ext4 /dev/nvme0n1p1 /mnt/ssd_cache
+
+  elif [ "$HOSTS" = "c3n25" ]
+  then
+  # umount -l /data1/sungho/trash
+  umount -l /mnt/ssd_cache
+  sudo mount -t ext4 /dev/nvme0n1p1 /mnt/ssd_cache
   sudo kill -9 `pidof ssfs`
   fi
 
