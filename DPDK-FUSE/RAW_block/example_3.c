fefa729f7801cec26af6ea3129732b4ea9c591f4
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-28 16:57:37 -0700

    add

--- DPDK-FUSE/RAW_block/example_3.c
+++ DPDK-FUSE/RAW_block/example_3.c
@@ -42,7 +42,7 @@ int main()
     printf("BEFORE READ END\n");
 
 
-    fd = open(fl_nm, O_RDWR|O_CREAT);
+    fd = open(fl_nm, O_RDWR|O_CREAT|O_DIRECT);
     pwrite(fd, aligned_buf_w, 4089, 0);
 
     //error checking for close process
