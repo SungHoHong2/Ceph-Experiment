d808e8cfe547574bb33f82ea6097d3f0874d3875
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-27 20:18:10 -0700

    add

--- DPDK-FUSE/RAW_block/example.c
+++ DPDK-FUSE/RAW_block/example.c
@@ -1,34 +1,61 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <time.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <inttypes.h>
-#include <sys/types.h>
-#include <sys/queue.h>
-#include <netinet/in.h>
-#include <setjmp.h>
-#include <stdarg.h>
-#include <ctype.h>
-#include <errno.h>
-#include <getopt.h>
-#include <signal.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <math.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <mtd/mtd-user.h>
 
 
 int main(){
-    // fd = open(argv[1], O_RDONLY | O_DIRECT);
-    int fd = open("/mnt/ssd_cache", O_RDWR | O_DIRECT);
 
-    
+    mtd_info_t mtd_info;           // the MTD structure
+    erase_info_t ei;               // the erase block structure
+    int i;
+
+    unsigned char data[20] = { 0xDE, 0xAD, 0xBE, 0xEF,  // our data to write
+                               0xDE, 0xAD, 0xBE, 0xEF,
+                               0xDE, 0xAD, 0xBE, 0xEF,
+                               0xDE, 0xAD, 0xBE, 0xEF,
+                               0xDE, 0xAD, 0xBE, 0xEF};
+
+    unsigned char read_buf[20] = {0x00};
 
 
 
-    printf("howdy\n");
+    int fd = open("/mnt/ssd_cache", O_RDWR | O_DIRECT); // get the device info
+    printf("MTD Type: %x\nMTD total size: %x bytes\nMTD erase size: %x bytes\n",
+           mtd_info.type, mtd_info.size, mtd_info.erasesize);
+
+
+    ei.length = mtd_info.erasesize;   //set the erase block size
+
+    for(ei.start = 0; ei.start < mtd_info.size; ei.start += ei.length)
+    {
+        ioctl(fd, MEMUNLOCK, &ei);
+        // printf("Eraseing Block %#x\n", ei.start); // show the blocks erasing
+        // warning, this prints a lot!
+        ioctl(fd, MEMERASE, &ei);
+    }
+
+    lseek(fd, 0, SEEK_SET);
+    read(fd, read_buf, sizeof(read_buf));
+
+    // sanity check, should be all 0xFF if erase worked
+    for(i = 0; i<20; i++)
+        printf("buf[%d] = 0x%02x\n", i, (unsigned int)read_buf[i]);
+
+
+    lseek(fd, 0, SEEK_SET);        // go back to first block's start
+    write(fd, data, sizeof(data)); // write our message
+
+
+    lseek(fd, 0, SEEK_SET);              // go back to first block's start
+    read(fd, read_buf, sizeof(read_buf));// read the data
+
+
+    // sanity check, now you see the message we wrote!
+    for(i = 0; i<20; i++)
+        printf("buf[%d] = 0x%02x\n", i, (unsigned int)read_buf[i]);
+
+
+    close(fd);
     return 0;
 }
