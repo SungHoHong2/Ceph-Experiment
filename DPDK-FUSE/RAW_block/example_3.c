01cca5ae36802b1a3d8360acb5ebbbe86281af0a
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-28 16:51:31 -0700

    add

--- DPDK-FUSE/RAW_block/example_3.c
+++ DPDK-FUSE/RAW_block/example_3.c
@@ -52,8 +52,8 @@ int main()
         printf("[succeeded in close]\n");
     }
 
-    // fd = open(fl_nm, O_RDWR|O_CREAT, 0777);
-    fd = open(fl_nm, O_RDWR|O_CREAT|O_DIRECT, 0777);
+    fd = open(fl_nm, O_RDWR|O_CREAT, 0777);
+    // fd = open(fl_nm, O_RDWR|O_CREAT|O_DIRECT, 0777);
     nr = pread(fd, aligned_buf_r, 4089, 0);
 
     printf("AFTER READ BEGIN\n");
