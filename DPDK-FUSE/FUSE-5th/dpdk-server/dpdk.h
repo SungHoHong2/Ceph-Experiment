02803c0c870e0be99408cb949a9b209c4ac03fe0
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-27 23:31:15 -0700

    add

--- DPDK-FUSE/FUSE-5th/dpdk-server/dpdk.h
+++ DPDK-FUSE/FUSE-5th/dpdk-server/dpdk.h
@@ -424,16 +424,29 @@ int dpdk_init(){
 
     printf("MAC updating %s\n", mac_updating ? "enabled" : "disabled");
 
+//    /* create the mbuf pool */
+//    l2fwd_pktmbuf_pool = rte_pktmbuf_pool_create("mbuf_pool", NB_MBUF,
+//                                                 MEMPOOL_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE,
+//                                                 rte_socket_id());
+//
+//    /* create memory pool for send data */
+//    if (test_pktmbuf_pool == NULL) {
+//        test_pktmbuf_pool = rte_pktmbuf_pool_create("test_pktmbuf_pool",
+//                                                    NB_MBUF, MEMPOOL_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+//    }
+
     /* create the mbuf pool */
     l2fwd_pktmbuf_pool = rte_pktmbuf_pool_create("mbuf_pool", NB_MBUF,
-                                                 MEMPOOL_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE,
+                                                 MEMPOOL_CACHE_SIZE, 0, 4089,
                                                  rte_socket_id());
 
     /* create memory pool for send data */
     if (test_pktmbuf_pool == NULL) {
         test_pktmbuf_pool = rte_pktmbuf_pool_create("test_pktmbuf_pool",
-                                                    NB_MBUF, MEMPOOL_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+                                                    NB_MBUF, MEMPOOL_CACHE_SIZE, 0, 4089, rte_socket_id());
     }
+
+
 
     if (l2fwd_pktmbuf_pool == NULL)
         rte_exit(EXIT_FAILURE, "Cannot init mbuf pool\n");
