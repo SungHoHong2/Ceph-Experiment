b1781c1acd7fb04373655a50e6fb6e1779c2e917
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-28 17:39:11 -0700

    add

--- DPDK-FUSE/FUSE-5th/dpdk-server/dpdk.h
+++ DPDK-FUSE/FUSE-5th/dpdk-server/dpdk.h
@@ -136,7 +136,7 @@ dpdk_packet_hexdump(FILE *f, const char * title, const void * buf, unsigned int 
 //            msg = &obj;
 //            strncpy(obj.data, sdata, 26);
 
-            
+
 
 
         }
--- DPDK-FUSE/FUSE-5th/dpdk-server/main.c
+++ DPDK-FUSE/FUSE-5th/dpdk-server/main.c
@@ -60,6 +60,8 @@ main(int argc, char **argv)
 	if(argc>1){
 		int i;
 		for(i=0;i<argc; i++){
+			printf("%s\n",argv[i]);
+
 			if(strcmp(argv[i],"NOFILESYSTEM")==0){
 				NOFILESYSTEM = 1;
 			}
