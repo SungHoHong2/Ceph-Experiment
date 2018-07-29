085fffc230eb26e770526c80ce0c1c276c5e6b19
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-28 19:09:29 -0700

    add

--- DPDK-FUSE/FUSE-5th/dpdk_client/ssfs_fuse.h
+++ DPDK-FUSE/FUSE-5th/dpdk_client/ssfs_fuse.h
@@ -306,20 +306,20 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
         total_requests++;
     }
 
-    while(rte_ring_dequeue(rx_ring, &msg) < 0){
-        usleep(5);
-    }
-
-    _msg = (struct message *)msg;
-//     printf("CHARA Received '%s'\n", _msg->data);
-    av = TAILQ_FIRST(&avg_queue);
-    av->end_time = getTimeStamp();
-    av->interval = av->end_time - av->start_time;
-    printf("[%ld] recv msg in FUSE: %ld :: %ld\n", av->num, strlen(_msg->data), av->interval);
-    intervals[test_i] = (double)av->interval;
-    test_i++;
-    TAILQ_REMOVE(&avg_queue, av, nodes);
-    free(av);
+//    while(rte_ring_dequeue(rx_ring, &msg) < 0){
+//        usleep(5);
+//    }
+//
+//    _msg = (struct message *)msg;
+////     printf("CHARA Received '%s'\n", _msg->data);
+//    av = TAILQ_FIRST(&avg_queue);
+//    av->end_time = getTimeStamp();
+//    av->interval = av->end_time - av->start_time;
+//    printf("[%ld] recv msg in FUSE: %ld :: %ld\n", av->num, strlen(_msg->data), av->interval);
+//    intervals[test_i] = (double)av->interval;
+//    test_i++;
+//    TAILQ_REMOVE(&avg_queue, av, nodes);
+//    free(av);
 
 
     if(total_requests==MAX_LOOP){
