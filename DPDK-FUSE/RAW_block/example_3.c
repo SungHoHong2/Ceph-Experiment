0801fcdc30b9fae6a48711e6b516a2e9e2ce243b
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-28 17:00:05 -0700

    add

--- DPDK-FUSE/RAW_block/example_3.c
+++ DPDK-FUSE/RAW_block/example_3.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#define BUF_SIZE 16
 
 int main()
 {
@@ -18,7 +19,7 @@ int main()
     char* aligned_buf_r = NULL;
 
     void* ad = NULL;
-    if (posix_memalign(&ad, 32, 4089)) {
+    if (posix_memalign(&ad, 32, BUF_SIZE)) {
         perror("posix_memalign failed"); exit (EXIT_FAILURE);
     }
 
@@ -26,7 +27,7 @@ int main()
 
 
     ad = NULL;
-    if (posix_memalign(&ad, 32, 4089)) {
+    if (posix_memalign(&ad, 32, BUF_SIZE)) {
         perror("posix_memalign failed"); exit (EXIT_FAILURE);
     }
     aligned_buf_r = (char *)(ad);
@@ -43,18 +44,9 @@ int main()
 
 
     fd = open(fl_nm, O_RDWR|O_CREAT|O_DIRECT);
-    pwrite(fd, aligned_buf_w, 4089, 0);
+    pwrite(fd, aligned_buf_w, BUF_SIZE, 0);
 
-    //error checking for close process
-//    if(close(fd) == -1){
-//        perror("[error in close]\n");
-//    }else{
-//        printf("[succeeded in close]\n");
-//    }
-
-//    fd = open(fl_nm, O_RDWR|O_CREAT, 0777);
-//    fd = open(fl_nm, O_RDONLY|O_DIRECT);
-    nr = pread(fd, aligned_buf_r, 4089, 0);
+    pread(fd, aligned_buf_r, BUF_SIZE, 0);
 
     printf("AFTER READ BEGIN\n");
     printf("\taligned_buf_r::%s\n",aligned_buf_r);
