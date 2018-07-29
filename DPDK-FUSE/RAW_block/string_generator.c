f96a8d32e7602a7012a3f55964fe27749dadab5e
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-28 21:53:11 -0700

    add

--- DPDK-FUSE/RAW_block/string_generator.c
+++ DPDK-FUSE/RAW_block/string_generator.c
@@ -8,7 +8,6 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #define BUF_SIZE 4096
-#define PKT_SIZE 1024
 struct message {
     char data[PKT_SIZE];
 };
@@ -22,78 +21,59 @@ int main()
     char* aligned_buf_r = NULL;
 
     void* ad = NULL;
-//    if (posix_memalign(&ad, 32, BUF_SIZE)) {
-//        perror("posix_memalign failed"); exit (EXIT_FAILURE);
-//    }
-//
-//    aligned_buf_w = (char *)(ad);
-//
-//
-//    ad = NULL;
-//    if (posix_memalign(&ad, 32, BUF_SIZE)) {
-//        perror("posix_memalign failed"); exit (EXIT_FAILURE);
-//    }
-//    aligned_buf_r = (char *)(ad);
-//
-//    memset(aligned_buf_w, '*', BUF_SIZE * sizeof(char));
-//
-////    printf("BEFORE READ BEGIN\n");
-////    printf("\t aligned_buf_w::%s\n",aligned_buf_w);
-////    printf("\t aligned_buf_r::%s\n",aligned_buf_r);
-////    printf("BEFORE READ END\n");
-//
-//
-//    fd = open(fl_nm, O_RDWR|O_CREAT);
-//    // fd = open(fl_nm, O_RDWR|O_CREAT|O_DIRECT);
-//    pwrite(fd, aligned_buf_w, BUF_SIZE, 0);
-//
+    if (posix_memalign(&ad, 32, BUF_SIZE)) {
+        perror("posix_memalign failed"); exit (EXIT_FAILURE);
+    }
 
-//    pread(fd, aligned_buf_r, BUF_SIZE, 0);
+    aligned_buf_w = (char *)(ad);
+
+
+    ad = NULL;
+    if (posix_memalign(&ad, 32, BUF_SIZE)) {
+        perror("posix_memalign failed"); exit (EXIT_FAILURE);
+    }
+    aligned_buf_r = (char *)(ad);
+
+    memset(aligned_buf_w, '*', BUF_SIZE * sizeof(char));
+
+//    printf("BEFORE READ BEGIN\n");
+//    printf("\t aligned_buf_w::%s\n",aligned_buf_w);
+//    printf("\t aligned_buf_r::%s\n",aligned_buf_r);
+//    printf("BEFORE READ END\n");
+
+
+    fd = open(fl_nm, O_RDWR|O_CREAT);
+    // fd = open(fl_nm, O_RDWR|O_CREAT|O_DIRECT);
+    pwrite(fd, aligned_buf_w, BUF_SIZE, 0);
+
+
+    pread(fd, aligned_buf_r, BUF_SIZE, 0);
 //    printf("AFTER READ BEGIN\n");
-//    printf("\taligned_buf_r::%ld\n",strlen(aligned_buf_r));
+    printf("\taligned_buf_r::%ld\n",strlen(aligned_buf_r));
 //    printf("AFTER READ END\n");
 
 
     //error checking for close process
-//    if(close(fd) == -1){
-//        perror("[error in close]\n");
-//    }else{
-//        printf("[succeeded in close]\n");
-//    }
+    if(close(fd) == -1){
+        perror("[error in close]\n");
+    }else{
+        printf("[succeeded in close]\n");
+    }
 
 
-//    printf("splitting string into 1024 bytes\n");
-//
-//
-//    char array_test[3][1024];
-//
-//    int i;
-//    for(i=0; i<3; i++){
-//        memcpy(array_test[i], aligned_buf_r, 1024);
-//        printf("%s\n", array_test[i]);
-//        aligned_buf_r+=1024;
-//    }
-int i;
-
-    struct message objs[4];
-
-//    char* aligned_buf_r = NULL;
-    aligned_buf_r = (char *)(ad);
-    fd = open(fl_nm, O_RDWR|O_CREAT, 0777);
-    nr = pread(fd, aligned_buf_r, BUF_SIZE, 0);
-    close(fd);
-
-    printf("%s",aligned_buf_r);
-
-//    printf("CHARA:: send msg in FILESYSTEM: %ld\n", strlen(aligned_buf_r));
-
-//    for(i=0; i<3; i++){
-//        memcpy(objs[i].data, aligned_buf_r, PKT_SIZE);
-//        printf("cHARA: merged msg in DPDK: %ld\n", strlen(objs[i].data));
-//        aligned_buf_r+=PKT_SIZE;
-//    }
+    printf("splitting string into 1024 bytes\n");
 
 
+    char array_test[3][1024];
+
+    int i;
+    for(i=0; i<3; i++){
+        memcpy(array_test[i], aligned_buf_r, 1024);
+        printf("%s\n", array_test[i]);
+        aligned_buf_r+=1024;
+    }
+
+    
 
 
 
