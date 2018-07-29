64a89c36e2067ab91e3b2f8e30fc4be79054a6b8
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-28 20:51:17 -0700

    add

--- DPDK-FUSE/FUSE-5th/dpdk_client/ssfs_dpdk.h
+++ DPDK-FUSE/FUSE-5th/dpdk_client/ssfs_dpdk.h
@@ -9,7 +9,7 @@ static int mac_updating = 1;
 
 #define NB_MBUF   8192
 
-#define MAX_PKT_BURST 32
+#define MAX_PKT_BURST 4
 //#define MAX_PKT_BURST 1
 
 #define BURST_TX_DRAIN_US 100 /* TX drain every ~100us */
