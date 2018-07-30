30d079c71ce06f9423f86e3abac1ef3942fc6d81
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-29 19:12:26 -0700

    add

--- DPDK-FUSE/RAW_block/string_generator.c
+++ DPDK-FUSE/RAW_block/string_generator.c
@@ -10,6 +10,38 @@
 #define BUF_SIZE 1024
 #define SECTOR 512
 
+
+
+
+void divideString(char *str, int n)
+{
+    int str_size = strlen(str);
+    int i;
+    int part_size;
+
+// Check if string can be divided in
+// n equal parts
+    if (str_size % n != 0)
+    {
+        printf("Invalid Input: String size");
+        printf(" is not divisible by n");
+        return;
+    }
+
+// Calculate the size of parts to
+// find the division points
+    part_size = str_size / n;
+    for (i = 0; i< str_size; i++)
+    {
+        if (i % part_size == 0)
+            printf("\n");
+        printf("%c", str[i]);
+    }
+}
+
+
+
+
 int main()
 {
 
@@ -66,11 +98,38 @@ int main()
     int end_size = 1024;
 
 
-    for(i=0; i<4; i++){
-        memcpy(array_test[i], aligned_buf_r, end_size);
-        printf("%ld\n", strlen(array_test[i]));
-        aligned_buf_r+=1024;
-    }
+    char *p1 = malloc( sizeof(char) * 1024);
+    char *p2 = malloc( sizeof(char) * 1024);
+    char *p3 = malloc( sizeof(char) * 1024);
+    char *p4 = malloc( sizeof(char) * 1024);
+
+    memcpy(p1, aligned_buf_r, end_size);
+    printf("%ld\n", strlen(p1));
+    aligned_buf_r+=1024;
+
+    memcpy(p2, aligned_buf_r, end_size);
+    printf("%ld\n", strlen(p2));
+    aligned_buf_r+=1024;
+
+    memcpy(p3, aligned_buf_r, end_size);
+    printf("%ld\n", strlen(p3));
+    aligned_buf_r+=1024;
+
+    memcpy(p4, aligned_buf_r, end_size);
+    printf("%ld\n", strlen(p4));
+    aligned_buf_r+=1024;
+
+    free(p1);
+
+    aligned_buf_r-=4096;
+
+
+//    for(i=0; i<4; i++){
+//        memcpy(array_test[i], aligned_buf_r, end_size);
+//        printf("%ld\n", strlen(array_test[i]));
+//        aligned_buf_r+=1024;
+//    }
+
 
 
     aligned_buf_r = NULL;
@@ -87,12 +146,37 @@ int main()
     close(fd);
     printf("\t aligned_buf_r::%ld\n",strlen(aligned_buf_r));
 
-    
-    for(i=0; i<4; i++){
-        memcpy(array_test[i], aligned_buf_r, end_size);
-        printf("%ld\n", strlen(array_test[i]));
-        aligned_buf_r+=1024;
-    }
+
+//    for(i=0; i<4; i++){
+//        memcpy(array_test[i], aligned_buf_r, end_size);
+//        printf("%ld\n", strlen(array_test[i]));
+//        aligned_buf_r+=1024;
+//    }
+
+
+    p1 = malloc( sizeof(char) * 1024);
+    p2 = malloc( sizeof(char) * 1024);
+    p3 = malloc( sizeof(char) * 1024);
+    p4 = malloc( sizeof(char) * 1024);
+
+    memcpy(p1, aligned_buf_r, end_size);
+    printf("%ld\n", strlen(p1));
+    aligned_buf_r+=1024;
+
+    memcpy(p2, aligned_buf_r, end_size);
+    printf("%ld\n", strlen(p2));
+    aligned_buf_r+=1024;
+
+    memcpy(p3, aligned_buf_r, end_size);
+    printf("%ld\n", strlen(p3));
+    aligned_buf_r+=1024;
+
+    memcpy(p4, aligned_buf_r, end_size);
+    printf("%ld\n", strlen(p4));
+    aligned_buf_r+=1024;
+
+
+
 //
 //    aligned_buf_r-=4096;
 //    end_size-=4096;
