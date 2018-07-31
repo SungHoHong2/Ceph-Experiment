151711ce63f36ae89b1312740c27bd5c5de6f8d0
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-30 20:41:54 -0700

    add

--- DPDK-FUSE/version.sh
+++ DPDK-FUSE/version.sh
@@ -44,6 +44,8 @@ then
     then
     umount -l /mnt/hdd
     sudo kill -9 `pidof ssfs`
+    cd /data1/sungho/Ceph-Experiment/DPDK-FUSE/FUSE-5th/posix_client
+    gcc -D _BSD_SOURCE ssfs.c -o ssfs -lm -D_FILE_OFFSET_BITS=64 -I/usr/local/include/fuse3 -L/usr/local/lib/x86_64-linux-gnu -lfuse3 -lpthread
     fi
   fi
 
@@ -138,7 +140,7 @@ gcc ssfs.c -o ssfs -lpthread
 elif [ "$HOSTS" = "c3n25" ]
 then
 cd /data1/sungho/Ceph-Experiment/DPDK-FUSE/FUSE-5th/posix_client
-gcc -D _BSD_SOURCE ssfs.c -o ssfs -lm -D_FILE_OFFSET_BITS=64 -I/usr/local/include/fuse3 -L/usr/local/lib/x86_64-linux-gnu -lfuse3 -lpthread
+# gcc -D _BSD_SOURCE ssfs.c -o ssfs -lm -D_FILE_OFFSET_BITS=64 -I/usr/local/include/fuse3 -L/usr/local/lib/x86_64-linux-gnu -lfuse3 -lpthread
 ./ssfs -f /mnt/hdd  $1 $2 $3 $4
 # ./ssfs -f /mnt/ssd_cache "CACHE_HIT"  "HUNDRED"
 
