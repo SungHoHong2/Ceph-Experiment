4b6205571043a344bb054d500d0cd21081ed4e06
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-30 21:14:34 -0700

    add

--- DPDK-FUSE/version.sh
+++ DPDK-FUSE/version.sh
@@ -142,7 +142,7 @@ then
 cd /data1/sungho/Ceph-Experiment/DPDK-FUSE/FUSE-5th/posix_client
 # gcc -D _BSD_SOURCE ssfs.c -o ssfs -lm -D_FILE_OFFSET_BITS=64 -I/usr/local/include/fuse3 -L/usr/local/lib/x86_64-linux-gnu -lfuse3 -lpthread
 ./ssfs -f /mnt/hdd  $1 $2 $3 $4
-# ./ssfs -f /mnt/ssd_cache "CACHE_HIT"  "HUNDRED"
+# ./ssfs -f /mnt/ssd_cache "CACHE_HIT" "HUNDRED"
 
 fi
 
