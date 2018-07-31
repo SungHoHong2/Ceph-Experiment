7969dd83656bf1014d716ad6f9e8f78ffa1063f7
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-07-30 19:27:36 -0700

    add

--- DPDK-FUSE/FUSE-5th/posix_client/ssfs_fuse.h
+++ DPDK-FUSE/FUSE-5th/posix_client/ssfs_fuse.h
@@ -21,7 +21,7 @@ void calculateSD(double data[])
     for(i=0; i<max_loop; ++i)
         standardDeviation += pow(data[i] - mean, 2);
 
-    printf("std: %f\n",sqrt(standardDeviation/MAX_LOOP));
+    printf("std: %f\n",sqrt(standardDeviation/max_loop));
 }
 
 
@@ -329,7 +329,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
         e = NULL;
     }
 
-    if(total_requests==MAX_LOOP){
+    if(total_requests==max_loop){
         calculateSD(intervals);
     }
 
