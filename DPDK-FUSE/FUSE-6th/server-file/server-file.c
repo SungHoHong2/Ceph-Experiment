6db39a65190cfbc6c5594699cef4605d53bdc157
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-08-04 16:47:37 -0700

    add

--- DPDK-FUSE/FUSE-6th/server-file/server-file.c
+++ DPDK-FUSE/FUSE-6th/server-file/server-file.c
@@ -23,27 +23,107 @@
 #define PKT_SIZE 1024
 #define MERGE_PACKETS 4
 #define LOOPS 1000
+double intervals[99999];
 
+void calculateSD(double data[])
+{
+    float sum = 0.0, mean, standardDeviation = 0.0;
+    int i;
+    for(i=0; i<max_loop; ++i)
+    {
+        sum += data[i];
+    }
+
+    mean = sum/max_loop;
+    printf("mean: %f\n",mean);
+
+
+    for(i=0; i<max_loop; ++i)
+        standardDeviation += pow(data[i] - mean, 2);
+
+    printf("std: %f\n",sqrt(standardDeviation/max_loop));
+}
+
+struct avg_node
+{
+    uint64_t num;
+    uint64_t start_time;
+    uint64_t end_time;
+    uint64_t interval;
+    TAILQ_ENTRY(avg_node) nodes;
+};
+
+uint64_t getTimeStamp() {
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
+}
+
+
+
+
+void calculateSD(double data[])
+{
+    float sum = 0.0, mean, standardDeviation = 0.0;
+    int i;
+    for(i=0; i<max_loop; ++i)
+    {
+        sum += data[i];
+    }
+
+    mean = sum/max_loop;
+    printf("mean: %f\n",mean);
+
+
+    for(i=0; i<max_loop; ++i)
+        standardDeviation += pow(data[i] - mean, 2);
+
+    printf("std: %f\n",sqrt(standardDeviation/max_loop));
+}
 
 
 int main(){
 
-
-    int fd, nr;
+    int fd, nr, test_i=0;
     char* aligned_buf_r = NULL;
     void* ad = NULL;
+    struct avg_node *av = NULL;
+    char* aggregated = malloc(MERGE_PACKETS* PKT_SIZE * sizeof(char));
 
-    if (posix_memalign(&ad, SECTOR, PKT_SIZE * MERGE_PACKETS )) {
-        perror("posix_memalign failed"); exit (EXIT_FAILURE);
-    }
+    TAILQ_HEAD(avg_head, avg_node) avg_queue;
+    TAILQ_INIT(&avg_queue);
 
-    aligned_buf_r = (char *)(ad);
-    fd = open("/dev/nvme0n1p1", O_RDWR | O_DIRECT);
-    nr = pread(fd, aligned_buf_r, PKT_SIZE * MERGE_PACKETS, 0);
-    close(fd);
 
-    printf("%s\n",aligned_buf_r);
 
+//     for(int i =0; i<LOOPS; i ++) {
+
+        av = malloc(sizeof(struct avg_node));
+        av->start_time = getTimeStamp();
+        TAILQ_INSERT_TAIL(&avg_queue, av, nodes);
+
+
+        if (posix_memalign(&ad, SECTOR, PKT_SIZE * MERGE_PACKETS)) {
+            perror("posix_memalign failed");
+            exit(EXIT_FAILURE);
+        }
+
+        aligned_buf_r = (char *) (ad);
+        fd = open("/dev/nvme0n1p1", O_RDWR | O_DIRECT);
+        nr = pread(fd, aligned_buf_r, PKT_SIZE * MERGE_PACKETS, 0);
+        close(fd);
+
+        strcpy(aggregated, aligned_buf_r);
+
+        av = TAILQ_FIRST(&avg_queue);
+        av->end_time = getTimeStamp();
+        av->interval = av->end_time - av->start_time;
+        intervals[test_i] = (double) av->interval;
+        TAILQ_REMOVE(&avg_queue, av, nodes);
+        test_i++;
+        printf("[%ld] recv msg in FUSE: %ld :: %ld\n", av->num, strlen(aggregated), av->interval);
+  //  }
+
+    calculateSD(intervals);
 
     return 0;
 
